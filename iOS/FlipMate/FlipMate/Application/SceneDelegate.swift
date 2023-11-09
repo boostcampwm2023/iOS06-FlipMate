9a4de0f3a71855bbbda72e0deaabb1e89d90abda
ericKwon95 <ericyong95@gmail.com>
2023-11-09 18:49:11 +0900

    chore: 프로젝트 초기 설정
    - 클린 아키텍처 + MVVM 구조를 사용한 폴더링 적용, iOS 최소 지원 버전 설정

---
+++ iOS/FlipMate/FlipMate/Application/SceneDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  SceneDelegate.swift
+//  FlipMate
+//
+//  Created by 권승용 on 11/9/23.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+    
+    var window: UIWindow?
+    
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        guard let windowScene = (scene as? UIWindowScene) else {
+            return
+        }
+        
+        let window = UIWindow(windowScene: windowScene)
+        window.rootViewController = ViewController()
+        window.makeKeyAndVisible()
+        
+        self.window = window
+    }
+}
+
