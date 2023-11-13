c3d7079a57a949dfcc758d5dc0bb03071febd388
nemanjabenkovic <evolroop@naver.com>
2023-11-13 17:20:31 +0900

    Revert "iOS / feature: RootViewController 재구성 및 TabBarController 추가"
    This reverts commit 65027f47f3ec4598167eda99091f0925ad580762.

--- iOS/FlipMate/FlipMate/Application/SceneDelegate.swift
+++ iOS/FlipMate/FlipMate/Application/SceneDelegate.swift
@@ -8,37 +8,19 @@
 import UIKit
 
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
-
+    
     var window: UIWindow?
-
+    
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         guard let windowScene = (scene as? UIWindowScene) else {
             return
         }
-
+        
         let window = UIWindow(windowScene: windowScene)
-        
-        
-        let timerViewController = UINavigationController(rootViewController: ViewController())
-        let socialViewController = UINavigationController(rootViewController: ViewController())
-        let chartViewController = UINavigationController(rootViewController: ViewController())
-        
-        let tabBarController = UITabBarController()
-        tabBarController.setViewControllers([timerViewController, socialViewController, chartViewController], animated: true)
-        
-        if let items = tabBarController.tabBar.items {
-            items[0].selectedImage = UIImage(systemName: "person.3.fill")
-            items[0].image = UIImage(systemName: "person.3")
-            
-            items[1].selectedImage = UIImage(systemName: "timer")
-            items[1].image = UIImage(systemName: "timer")
-            
-            items[2].selectedImage = UIImage(systemName: "chart.bar.fill")
-            items[2].image = UIImage(systemName: "chart.bar")
-        }
-        
-        window.rootViewController = tabBarController
+        window.rootViewController = ViewController()
         window.makeKeyAndVisible()
+        
         self.window = window
     }
 }
+
