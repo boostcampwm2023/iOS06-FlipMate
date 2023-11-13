49fe67de82e3e10e7efe2c28b164be05ea464ff8
leemhyungyu <lhglbkksj@naver.com>
2023-11-13 16:49:35 +0900

    feat: LoginViewController 생성
    - LoginViewController 생성 후 RootViewController 연결

---
+++ iOS/FlipMate/FlipMate/Presentation/LoginScene/ViewController/LoginViewController.swift
@@ -0,0 +1,14 @@
+//
+//  LoginViewController.swift
+//  FlipMate
+//
+//  Created by 임현규 on 2023/11/13.
+//
+
+import UIKit
+
+final class LoginViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
