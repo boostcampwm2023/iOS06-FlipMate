6f03bbb767fdb5ccc1ddcb298409f253163e1217
임현규 <48830320+leemhyungyu@users.noreply.github.com>
2023-11-13 22:18:36 +0900

    [iOS#45] 비로그인 버튼 클릭시 화면 전환 구현 (#46)
    * feat: 소셜, 차트 ViewController 생성
    
    * feat: 비로그인 버튼 클릭시 타이머 화면으로 화면 전환

---
+++ iOS/FlipMate/FlipMate/Presentation/SocialScene/ViewController/SocialViewController.swift
@@ -0,0 +1,15 @@
+//
+//  SocialViewController.swift
+//  FlipMate
+//
+//  Created by 임현규 on 2023/11/13.
+//
+
+import UIKit
+
+final class SocialViewController: BaseViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
