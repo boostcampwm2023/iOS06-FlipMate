ee3420a06bca471bb9ad30f1a755d2b35fcebd29
leemhyungyu <lhglbkksj@naver.com>
2023-11-13 17:45:17 +0900

    feat: 구글, 애플 로그인 버튼 UI 구현
    - 각 로그인 버튼 타입에 맞게 버튼 UI 구현
    - 버튼 layer의 shadow설정

--- iOS/FlipMate/FlipMate/Presentation/LoginScene/ViewController/LoginViewController.swift
+++ iOS/FlipMate/FlipMate/Presentation/LoginScene/ViewController/LoginViewController.swift
@@ -8,6 +8,21 @@
 import UIKit
 
 final class LoginViewController: UIViewController {
+
+    // MARK: - UI Components
+    private var googleLoginButton: UIButton = {
+        let button = UIButton()
+        button.setLoginButton(type: .google)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
+
+    private var appleLoginButton: UIButton = {
+        let button = UIButton()
+        button.setLoginButton(type: .apple)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
     // MARK: - Life Cycle
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -19,5 +34,37 @@ final class LoginViewController: UIViewController {
 private extension LoginViewController {
     func configureUI() {
         view.backgroundColor = .systemBackground
+
+        [ googleLoginButton, appleLoginButton ].forEach { view.addSubview($0) }
+        
+        NSLayoutConstraint.activate([
+            googleLoginButton.bottomAnchor.constraint(equalTo: appleLoginButton.topAnchor, constant: -20),
+            googleLoginButton.leadingAnchor.constraint(equalTo: appleLoginButton.leadingAnchor),
+            googleLoginButton.trailingAnchor.constraint(equalTo: appleLoginButton.trailingAnchor),
+            googleLoginButton.heightAnchor.constraint(equalToConstant: 44),
+            
+            appleLoginButton.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -100),
+            appleLoginButton.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 30),
+            appleLoginButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -30),
+            appleLoginButton.heightAnchor.constraint(equalToConstant: 44)
+        ])
+    }
+}
+
+// MARK: - UIButton extension
+fileprivate extension UIButton {
+    func setLoginButton(type: LoginType) {
+        self.setTitle(type.description, for: .normal)
+        self.backgroundColor = .systemBackground
+        self.titleLabel?.font = .systemFont(ofSize: 16)
+        self.setTitleColor(.black, for: .normal)
+        self.titleLabel?.textColor = .black
+        self.layer.cornerRadius = 11
+        self.layer.borderWidth = 1.0
+        self.layer.borderColor = UIColor.systemGray2.cgColor
+        self.layer.shadowColor = UIColor.black.cgColor
+        self.layer.shadowOffset = CGSize(width: 3.0, height: 3.0)
+        self.layer.shadowOpacity = 0.3
+        self.layer.shadowRadius = 2.0
     }
 }
