3695f5a9ab1eb382eb3e2fa7f0bd421674022095
leemhyungyu <lhglbkksj@naver.com>
2023-11-14 16:27:29 +0900

    feat: DeviceOrientation 열거형 작성

---
+++ iOS/FlipMate/FlipMate/Presentation/TimerScene/Model/DeviceOrientation.swift
@@ -0,0 +1,18 @@
+//
+//  DeviceOrientation.swift
+//  FlipMate
+//
+//  Created by 임현규 on 2023/11/14.
+//
+
+import Foundation
+
+enum DeviceOrientation: Int {
+    case unknown
+    case portrait
+    case portraitUpsideDown
+    case landscapeLeft
+    case landscapeRight
+    case faceUp
+    case faceDown
+}
