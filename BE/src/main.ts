64be605d5a64e82df6c6043480b3435e8bd68b51
victolee0 <39608452+victolee0@users.noreply.github.com>
2023-11-15 14:06:16 +0900

    [BE#22] 타이머 화면 컨트롤러 작성 및 API문서화 (#73)
    * chore: study-logs API 문서화
    
    - swagger를 통해 study-logs 요청 문서화
    
    * chore: study-logs get api 문서 추가
    
    - study-logs get api 문서 추가

--- BE/src/main.ts
+++ BE/src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('StudyLog API')
+    .setDescription('StudyLog 애플리케이션 API 문서')
+    .setVersion('1.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
--- BE/src/study-logs/study-logs.controller.ts
+++ BE/src/study-logs/study-logs.controller.ts
@@ -1,13 +1,42 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { StudyLogsService } from './study-logs.service';
 import { StudyLogs } from './study-logs.entity';
+import {
+  ApiBadRequestResponse,
+  ApiCreatedResponse,
+  ApiOperation,
+  ApiTags,
+} from '@nestjs/swagger';
 
+@ApiTags('Timer')
 @Controller('study-logs')
 export class StudyLogsController {
   constructor(private readonly studyLogsService: StudyLogsService) {}
 
   @Post()
+  @ApiOperation({ summary: '학습시간 생성 및 종료' })
+  @ApiCreatedResponse({
+    type: StudyLogs,
+    description: '학습 기록이 성공적으로 생성되었습니다.',
+  })
+  @ApiBadRequestResponse({
+    description: '잘못된 요청입니다.',
+  })
   createStudyLogs(@Body() studyLogsData: StudyLogs): Promise<StudyLogs> {
     return this.studyLogsService.create(studyLogsData);
   }
+
+  @Get()
+  @ApiOperation({ summary: '학습시간 조회' })
+  @ApiCreatedResponse({
+    type: StudyLogs,
+    description: '학습 기록이 성공적으로 조회되었습니다.',
+  })
+  @ApiBadRequestResponse({
+    description: '잘못된 요청입니다.',
+  })
+  getStudyLogs(): Promise<StudyLogs[]> {
+    return this.studyLogsService.findAll();
+  }
+  
 }
