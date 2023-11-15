02b8ee707c00b488cd7a20d4712376c835aad7e4
임영빈 <yeim@im-yeongbin-ui-MacBookAir.local>
2023-11-15 12:18:14 +0900

    chore: study-logs API 문서화
    - swagger를 통해 study-logs 요청 문서화

--- BE/src/study-logs/study-logs.controller.ts
+++ BE/src/study-logs/study-logs.controller.ts
@@ -1,12 +1,27 @@
 import { Body, Controller, Post } from '@nestjs/common';
 import { StudyLogsService } from './study-logs.service';
 import { StudyLogs } from './study-logs.entity';
+import {
+  ApiBadRequestResponse,
+  ApiCreatedResponse,
+  ApiOperation,
+  ApiTags,
+} from '@nestjs/swagger';
 
+@ApiTags('Timer')
 @Controller('study-logs')
 export class StudyLogsController {
   constructor(private readonly studyLogsService: StudyLogsService) {}
 
   @Post()
+  @ApiOperation({ summary: '학습시간 생성 및 종료' })
+  @ApiCreatedResponse({
+    type: StudyLogs,
+    description: '학습 기록이 성공적으로 생성되었습니다.',
+  })
+  @ApiBadRequestResponse({
+    description: '잘못된 요청입니다.',
+  })
   createStudyLogs(@Body() studyLogsData: StudyLogs): Promise<StudyLogs> {
     return this.studyLogsService.create(studyLogsData);
   }
