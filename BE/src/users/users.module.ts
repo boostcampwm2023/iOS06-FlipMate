f4509abece0e6f87da313369d7cdffe4e8fec32a
Yeongbin Im <56269396+yeongbinim@users.noreply.github.com>
2023-11-16 18:57:07 +0900

    [BE#21] google OAuth 로그인 (#102)
    * feat: google 서버로부터 받아온 AccessToken기반 User생성
    
    - google OAuth 등록 및 세팅
    - DB에 없는 이메일이면 DB에 User생성
    
    * feat: 로그인 및 회원가입 요청 jwt토큰 반환
    
    - @nestjs/jwt 에서 jwtService.sign을 통해 jwt생성
    - loginWithGoogle service계층으로 이동

--- BE/src/users/users.module.ts
+++ BE/src/users/users.module.ts
@@ -2,9 +2,12 @@ import { Module } from '@nestjs/common';
 import { UsersController } from './users.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Users } from './users.entity';
+import { UsersService } from './users.service';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Users])],
+  exports: [UsersService, TypeOrmModule],
   controllers: [UsersController],
+  providers: [UsersService],
 })
 export class UsersModule {}
