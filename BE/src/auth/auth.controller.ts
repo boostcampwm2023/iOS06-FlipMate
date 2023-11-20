0c905645453bb6f3692c40a222b1a9aa352a3412
yeongbinim <yeim.dev@gmail.com>
2023-11-21 01:09:37 +0900

    feat: auth API문서화

--- BE/src/auth/auth.controller.ts
+++ BE/src/auth/auth.controller.ts
@@ -5,13 +5,23 @@ import { AuthGuard } from '@nestjs/passport';
 import { Response } from 'express';
 import { AccessTokenGuard } from './guard/bearer-token.guard';
 import { User } from 'src/users/decorator/user.decorator';
+import {
+  ApiBearerAuth,
+  ApiOperation,
+  ApiResponse,
+  ApiTags,
+} from '@nestjs/swagger';
 
+@ApiTags('auth')
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
   @Get('google')
   @UseGuards(AuthGuard('google'))
+  @ApiOperation({ summary: 'Google 로그인' })
+  @ApiResponse({ status: 200, description: '로그인 성공' })
+  @ApiResponse({ status: 401, description: '인증 실패' })
   googleAuth(@Req() req) {
     const user = req.user;
     return this.authService.loginWithGoogle(user);
@@ -19,6 +29,10 @@ export class AuthController {
 
   @Get('logout')
   @UseGuards(AccessTokenGuard)
+  @ApiOperation({ summary: '로그아웃' })
+  @ApiResponse({ status: 200, description: '로그아웃 성공' })
+  @ApiResponse({ status: 401, description: '인증 실패' })
+  @ApiBearerAuth()
   logout(@User('id') userId: number, @Res() res: Response) {
     console.log(`${userId}를 로그아웃 시키는 로직`);
     res.redirect('/');
