8d2f9c112de11fa5a9d0a97d80f7d0db895d5811
yeongbinim <yeim.dev@gmail.com>
2023-11-21 00:11:56 +0900

    feat: User Controller, Service계층 작성

--- BE/src/users/users.service.ts
+++ BE/src/users/users.service.ts
@@ -1,7 +1,8 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { UsersModel } from './entity/users.entity';
+import { CreateUserDto } from './dto/create-user.dto';
 
 @Injectable()
 export class UsersService {
@@ -10,17 +11,36 @@ export class UsersService {
     private usersRepository: Repository<UsersModel>,
   ) {}
 
-  async createUser(user: UserCreateDto): Promise<UsersModel> {
-    const users = this.usersRepository.create(user);
-    return this.usersRepository.save(users);
+  async createUser(user: CreateUserDto): Promise<UsersModel> {
+    try {
+      const userObject = this.usersRepository.create({
+        nickname: user.nickname,
+        email: user.email,
+        image_url: user.image_url,
+      });
+      return await this.usersRepository.save(userObject);
+    } catch (error) {
+      if (error.code === 'ER_DUP_ENTRY' || error.errno === 1062) {
+        throw new BadRequestException(
+          '닉네임 또는 이메일이 이미 사용 중입니다.',
+        );
+      }
+      throw error;
+    }
+  }
+
+  async isUniqueNickname(nickname: string): Promise<boolean> {
+    const isDuplicated = await this.usersRepository.exist({
+      where: { nickname },
+    });
+    return !isDuplicated;
   }
 
   async findUserByEmail(email: string): Promise<UsersModel> {
-    const user = await this.usersRepository.findOne({
+    const selectedUser = await this.usersRepository.findOne({
       where: { email },
     });
-    return user;
+
+    return selectedUser;
   }
 }
-
-export type UserCreateDto = {};
