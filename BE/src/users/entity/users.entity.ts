58f359ec73b04d15bf04065489e0caac7cc32f41
yeongbinim <yeim.dev@gmail.com>
2023-11-20 16:59:26 +0900

    feat: User엔티티 작성 및 relation 연결
    - classValidator를 설치하여 런타임에 검증
    - oneToMany로 study-logs와 categories를 연결

---
+++ BE/src/users/entity/users.entity.ts
@@ -0,0 +1,52 @@
+import { Entity, PrimaryGeneratedColumn, Column, OneToMany } from 'typeorm';
+import { IsEmail, IsString, Length } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
+import { StudyLogs } from 'src/study-logs/study-logs.entity';
+import { Categories } from 'src/categories/categories.entity';
+
+@Entity()
+export class UsersModel {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @ApiProperty({
+    type: 'string',
+    example: 'nickname123',
+    description: '유저 닉네임',
+  })
+  @Column({
+    length: 20,
+    unique: true,
+  })
+  @IsString()
+  @Length(1, 20)
+  nickname: string;
+
+  @ApiProperty({
+    type: 'string',
+    example: 'google_email@email.com',
+    description: 'OAuth로 로그인 한 구글 계정 아이디',
+  })
+  @Column({
+    unique: true,
+  })
+  @IsString()
+  @IsEmail()
+  google_email: string;
+
+  @ApiProperty({
+    type: 'string',
+    example: 'https://imgurl.com/path/file.png',
+    description: '유저 프로필 사진',
+  })
+  @Column({
+    nullable: true,
+  })
+  image_url: string;
+
+  @OneToMany(() => StudyLogs, (studyLog) => studyLog.user)
+  study_logs: StudyLogs[];
+
+  @OneToMany(() => Categories, (category) => category.user)
+  categories: Categories[];
+}
--- BE/src/users/users.service.ts
+++ BE/src/users/users.service.ts
@@ -1,23 +1,23 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
-import { Users } from './users.entity';
+import { UsersModel } from './entity/users.entity';
 
 @Injectable()
 export class UsersService {
   constructor(
-    @InjectRepository(Users)
-    private usersRepository: Repository<Users>,
+    @InjectRepository(UsersModel)
+    private usersRepository: Repository<UsersModel>,
   ) {}
 
-  async createUser(user: UserCreateDto): Promise<Users> {
+  async createUser(user: UserCreateDto): Promise<UsersModel> {
     const users = this.usersRepository.create(user);
     return this.usersRepository.save(users);
   }
 
-  async findUserByEmail(email: string): Promise<Users> {
+  async findUserByEmail(email: string): Promise<UsersModel> {
     const user = await this.usersRepository.findOne({
-      where: { google_id: email },
+      where: { google_email: email },
     });
     return user;
   }
