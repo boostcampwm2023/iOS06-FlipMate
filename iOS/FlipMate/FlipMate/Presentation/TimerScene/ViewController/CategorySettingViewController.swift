c6730ce1dfeb0989fa53ef71430251c77d5f64a8
leemhyungyu <lhglbkksj@naver.com>
2023-11-20 20:02:36 +0900

    feat: 카테고리 관리 ViewController 생성

---
+++ iOS/FlipMate/FlipMate/Presentation/TimerScene/ViewController/CategorySettingViewController.swift
@@ -0,0 +1,29 @@
+//
+//  CategorySettingViewController.swift
+//  FlipMate
+//
+//  Created by 임현규 on 2023/11/20.
+//
+
+import UIKit
+
+class CategorySettingViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
