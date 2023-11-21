916a602be51bac89a10e2b0c69361e2e6bf666c8
leemhyungyu <lhglbkksj@naver.com>
2023-11-21 15:21:59 +0900

    feat: Endpoint 구현

--- iOS/FlipMate/FlipMate/NetworkService/EndPoint.swift
+++ iOS/FlipMate/FlipMate/NetworkService/EndPoint.swift
@@ -6,3 +6,21 @@
 //
 
 import Foundation
+
+typealias ReqeustResponseable = Responsable & Requestable
+
+final class EndPoint<R: Decodable>: ReqeustResponseable {
+    typealias Response = R
+    
+    var baseURL: String
+    var path: String
+    var method: HTTPMethod
+    var bodyParameters: Encodable?
+    var headers: [String : String]?
+    
+    init(baseURL: String, path: String, method: HTTPMethod) {
+        self.baseURL = baseURL
+        self.path = path
+        self.method = method
+    }
+}
