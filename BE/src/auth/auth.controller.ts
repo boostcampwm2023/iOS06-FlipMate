e4f349aa65126a6d92d27a82da6d2d2c68137781
Yeongbin Im <56269396+yeongbinim@users.noreply.github.com>
2023-11-21 10:44:51 +0900

    [BE#121] 인가된 사용자인지 확인
    * feat: User Controller, Service계층 작성
    
    * feat: auth bearer-token 가드 작성
    
    - 인가된 사용자인지 token을 통해 검사하는 코드 작성
    
    * feat: User 프로퍼티 데코레이터 생성
    
    - 매번 const userId = req.user.id; 하기는 번거로워서 이를 직접 받을 수 있고, 특정값만 받도록 할 수 있는 커스텀 프로퍼티 데코레이터 생성
    
    * feat: auth API문서화

--- BE/src/auth/auth.controller.ts
+++ BE/src/auth/auth.controller.ts
@@ -2,25 +2,39 @@ import { AuthService } from './auth.service';
 
 import { Controller, Get, UseGuards, Req, Res } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
-import { Request, Response } from 'express';
+import { Response } from 'express';
+import { AccessTokenGuard } from './guard/bearer-token.guard';
+import { User } from 'src/users/decorator/user.decorator';
+import {
+  ApiBearerAuth,
+  ApiOperation,
+  ApiResponse,
+  ApiTags,
+} from '@nestjs/swagger';
 
+@ApiTags('auth')
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
-  @Get('')
-  @UseGuards(AuthGuard('google'))
-  async googleAuth() {} // Google 로그인 페이지로 리디렉션합니다
-
   @Get('google')
   @UseGuards(AuthGuard('google'))
-  googleAuthRedirect(@Req() req) {
+  @ApiOperation({ summary: 'Google 로그인' })
+  @ApiResponse({ status: 200, description: '로그인 성공' })
+  @ApiResponse({ status: 401, description: '인증 실패' })
+  googleAuth(@Req() req) {
     const user = req.user;
     return this.authService.loginWithGoogle(user);
   }
 
   @Get('logout')
-  logout(@Req() req: Request, @Res() res: Response) {
+  @UseGuards(AccessTokenGuard)
+  @ApiOperation({ summary: '로그아웃' })
+  @ApiResponse({ status: 200, description: '로그아웃 성공' })
+  @ApiResponse({ status: 401, description: '인증 실패' })
+  @ApiBearerAuth()
+  logout(@User('id') userId: number, @Res() res: Response) {
+    console.log(`${userId}를 로그아웃 시키는 로직`);
     res.redirect('/');
   }
 }
