4efd4e6b49e041aebdd4cb6460858dee45877e30
nemanjabenkovic <evolroop@naver.com>
2023-11-21 15:02:37 +0900

    feat: HTTPMethod 파일 추가

---
+++ iOS/FlipMate/FlipMate/NetworkService/HTTPMethod.swift
@@ -0,0 +1,15 @@
+//
+//  HttpMethod.swift
+//  FlipMate
+//
+//  Created by 신민규 on 11/21/23.
+//
+
+import Foundation
+
+enum HTTPMethod: String {
+    case get = "GET"
+    case post = "POST"
+    case delete = "DELETE"
+    case patch = "PATCH"
+}
