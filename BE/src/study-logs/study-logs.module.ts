f6abaff5c72166bf799fb929879e061dd72d651b
victolee0 <39608452+victolee0@users.noreply.github.com>
2023-11-22 23:09:59 +0900

    [BE#163] api 유저 id 헤더 토큰으로 받도록 수정 (#167)
    * fix: 오타 수정
    
    * chore: github action 확인용
    
    * chore: github action 확인을 위한 수정
    
    * fix: api 요청 시 유저 식별자를 헤더의 토큰으로 하도록 변경
    
    * fix: cd 조건문 조건 수정

--- BE/src/study-logs/study-logs.module.ts
+++ BE/src/study-logs/study-logs.module.ts
@@ -3,9 +3,11 @@ import { StudyLogsService } from './study-logs.service';
 import { StatsController, StudyLogsController } from './study-logs.controller';
 import { StudyLogs } from './study-logs.entity';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { AuthModule } from 'src/auth/auth.module';
+import { UsersModule } from 'src/users/users.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([StudyLogs])],
+  imports: [TypeOrmModule.forFeature([StudyLogs]), AuthModule, UsersModule],
   providers: [StudyLogsService],
   controllers: [StudyLogsController, StatsController],
 })
