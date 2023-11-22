4869205faab29d9b72d27f3361dcfb4134d1bb02
leemhyungyu <lhglbkksj@naver.com>
2023-11-22 17:07:17 +0900

    feat: DefaultTimerRepository 구현

--- iOS/FlipMate/FlipMate/Data/Repositories/DefaultTimerRepository.swift
+++ iOS/FlipMate/FlipMate/Data/Repositories/DefaultTimerRepository.swift
@@ -6,3 +6,57 @@
 //
 
 import Foundation
+import Combine
+
+final class DefaultTimerRepsoitory {
+    private let provider: Providable
+    
+    init(provider: Providable) {
+        self.provider = provider
+    }
+}
+
+extension DefaultTimerRepsoitory: TimerRepsoitory {
+    func startTimer(
+        startTime: Date,
+        userId: Int,
+        categoryId: Int)
+    -> AnyPublisher<Void, NetworkError> {
+        let requestDTO = TimerStartRequestDTO(
+            date: startTime.dateToString(format: .yyyyMMdd),
+            createdAt: startTime.dateToString(format: .yyyyMMddhhmmss),
+            type: "start",
+            learningTime: 0,
+            userID: userId,
+            categoryID: categoryId)
+        let endpoint = TimerEndpoints.startTimer(with: requestDTO)
+        
+        return provider.request(with: endpoint)
+            .map { response -> Void in
+                return ()
+            }
+            .eraseToAnyPublisher()
+    }
+    
+    func finishTimer(
+        endTime: Date,
+        learningTime: Int,
+        userId: Int,
+        categoryId: Int) 
+    -> AnyPublisher<Void, NetworkError> {
+        let requestDTO = TimerFinishRequestDTO(
+            date: endTime.dateToString(format: .yyyyMMdd),
+            createdAt: endTime.dateToString(format: .yyyyMMddhhmmss),
+            type: "end",
+            learningTime: learningTime,
+            userID: userId,
+            categoryID: categoryId)
+        let endpoint = TimerEndpoints.stopTimer(with: requestDTO)
+    
+        return provider.request(with: endpoint)
+            .map { response -> Void in
+                return ()
+            }
+            .eraseToAnyPublisher()
+    }
+}
