90fb8b870ba65dc73708f2cafaa9b9d83d9a98ca
leemhyungyu <lhglbkksj@naver.com>
2023-11-22 17:06:47 +0900

    feat: TimerRepository 포로토콜 구현

--- iOS/FlipMate/FlipMate/Domain/Repositories/TimerRepsoitory.swift
+++ iOS/FlipMate/FlipMate/Domain/Repositories/TimerRepsoitory.swift
@@ -6,3 +6,19 @@
 //
 
 import Foundation
+import Combine
+
+protocol TimerRepsoitory {
+    func startTimer(
+        startTime: Date,
+        userId: Int,
+        categoryId: Int
+    ) -> AnyPublisher<Void, NetworkError>
+    
+    func finishTimer(
+        endTime: Date,
+        learningTime: Int,
+        userId: Int,
+        categoryId: Int
+    ) -> AnyPublisher<Void, NetworkError>
+}
