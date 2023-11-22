54f6d75724ff4be72f6b137e815f1c49c8800421
yeongbinim <yeim.dev@gmail.com>
2023-11-22 22:52:52 +0900

    fix: categories api 유저 아이디 가드로 변경

--- BE/src/categories/categories.controller.ts
+++ BE/src/categories/categories.controller.ts
@@ -6,20 +6,22 @@ import {
   Param,
   Patch,
   Post,
-  Headers,
+  UseGuards,
 } from '@nestjs/common';
 import {
   ApiBadRequestResponse,
+  ApiBearerAuth,
   ApiCreatedResponse,
   ApiOperation,
   ApiParam,
   ApiTags,
 } from '@nestjs/swagger';
 import { CategoriesService } from './categories.service';
-import { CategoryGetDto } from './dto/request/get-categories.dto';
 import { CategoryCreateDto } from './dto/request/create-categories.dto';
 import { CategoryUpdateDto } from './dto/request/update-categories.dto';
 import { CategoryDto } from './dto/response/category.dto';
+import { AccessTokenGuard } from 'src/auth/guard/bearer-token.guard';
+import { User } from 'src/users/decorator/user.decorator';
 
 @ApiTags('Categories')
 @Controller('categories')
@@ -27,6 +29,8 @@ export class CategoriesController {
   constructor(private readonly categoriesService: CategoriesService) {}
 
   @Get()
+  @UseGuards(AccessTokenGuard)
+  @ApiBearerAuth()
   @ApiOperation({ summary: '카테고리 조회' })
   @ApiCreatedResponse({
     type: [CategoryDto],
@@ -35,14 +39,15 @@ export class CategoriesController {
   @ApiBadRequestResponse({
     description: '잘못된 요청입니다.',
   })
-  getCategories(
-    @Headers('authorization') CategoryGetDto,
-  ): Promise<CategoryDto[]> {
+  getCategories(@User('id') user_id: number): Promise<CategoryDto[]> {
     // TODO: 유저 id를 받아올 방식 정하기
+    user_id;
     return this.categoriesService.findByUserId(1);
   }
 
   @Post()
+  @UseGuards(AccessTokenGuard)
+  @ApiBearerAuth()
   @ApiOperation({ summary: '카테고리 생성' })
   @ApiCreatedResponse({
     type: CategoryDto,
@@ -52,12 +57,15 @@ export class CategoriesController {
     description: '잘못된 요청입니다.',
   })
   createCategories(
+    @User('id') user_id: number,
     @Body() categoriesData: CategoryCreateDto,
   ): Promise<CategoryCreateDto> {
-    return this.categoriesService.create(categoriesData);
+    return this.categoriesService.create(user_id, categoriesData);
   }
 
   @Patch(':category_id')
+  @UseGuards(AccessTokenGuard)
+  @ApiBearerAuth()
   @ApiOperation({ summary: '카테고리 수정' })
   @ApiParam({
     name: 'category_id',
@@ -73,13 +81,16 @@ export class CategoriesController {
     description: '잘못된 요청입니다.',
   })
   updateCategories(
+    @User('id') user_id: number,
     @Body() categoriesData: CategoryUpdateDto,
     @Param('category_id') category_id: number,
   ): Promise<CategoryDto> {
-    return this.categoriesService.update(categoriesData, category_id);
+    return this.categoriesService.update(user_id, categoriesData, category_id);
   }
 
   @Delete(':category_id')
+  @UseGuards(AccessTokenGuard)
+  @ApiBearerAuth()
   @ApiOperation({ summary: '카테고리 삭제' })
   @ApiParam({
     name: 'category_id',
@@ -93,7 +104,10 @@ export class CategoriesController {
   @ApiBadRequestResponse({
     description: '잘못된 요청입니다.',
   })
-  deleteCategories(@Param('category_id') category_id: number): Promise<void> {
-    return this.categoriesService.remove(category_id);
+  deleteCategories(
+    @User('id') user_id: number,
+    @Param('category_id') category_id: number,
+  ): Promise<void> {
+    return this.categoriesService.remove(user_id, category_id);
   }
 }
