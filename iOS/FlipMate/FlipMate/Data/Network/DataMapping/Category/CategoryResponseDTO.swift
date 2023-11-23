1c37447f291a5e69d11f252f584703e697422e99
ericKwon95 <ericyong95@gmail.com>
2023-11-23 11:40:57 +0900

    refactor: CategoryResponseDTO 변경
    - 변경된 API 요구사항에 맞게 변경

--- iOS/FlipMate/FlipMate/Data/Network/DataMapping/Category/CategoryResponseDTO.swift
+++ iOS/FlipMate/FlipMate/Data/Network/DataMapping/Category/CategoryResponseDTO.swift
@@ -8,12 +8,12 @@
 import Foundation
 
 struct CategoryResponseDTO: Decodable {
-    let id: Int
+    let categoryID: Int
     let name: String
     let colorCode: String
     
     private enum CodingKeys: String, CodingKey {
-        case id
+        case categoryID = "category_id"
         case name
         case colorCode = "color_code"
     }
