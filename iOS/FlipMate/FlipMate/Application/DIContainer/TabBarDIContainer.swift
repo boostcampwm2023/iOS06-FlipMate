4773e31b0fa9da6403c2c4d8491aa49096384f31
leemhyungyu <lhglbkksj@naver.com>
2023-11-26 03:28:37 +0900

    feat: TabBarDIContainer 구현

---
+++ iOS/FlipMate/FlipMate/Application/DIContainer/TabBarDIContainer.swift
@@ -0,0 +1,47 @@
+//
+//  TabBarDIContainer.swift
+//  FlipMate
+//
+//  Created by 임현규 on 2023/11/26.
+//
+
+import UIKit
+
+final class TabBarDIContainer: TabBarFlowCoordinatorDependencies {
+    
+    struct Dependencies {
+        let provider: Providable
+        let categoryManager: CategoryManager
+    }
+    
+    private let dependencies: Dependencies
+    
+    init(dependencies: Dependencies) {
+        self.dependencies = dependencies
+    }
+    
+    func makeTabBarController() -> UITabBarController {
+        return TabBarViewController()
+    }
+    
+    func makeTimerDIContainer() -> TimerSceneDIContainer {
+        let dependencies = TimerSceneDIContainer.Dependencies(
+            provider: dependencies.provider,
+            categoryManager: dependencies.categoryManager
+        )
+        
+        return TimerSceneDIContainer(dependencies: dependencies)
+    }
+    
+    
+    func makeTimerViewController() -> UIViewController {
+        return UIViewController()
+    }
+    
+    func makeTabBarFlowCoordinator(navigationController: UINavigationController) -> TabBarFlowCoordinator {
+        return TabBarFlowCoordinator(
+            navigationController: navigationController,
+            dependencies: self
+        )
+    }
+}
