7d452ca939446cc96a52d64530b6ffc86ce7b744
leemhyungyu <lhglbkksj@naver.com>
2023-11-26 00:18:14 +0900

    feat: LoginFlowCoordinator 구현

---
+++ iOS/FlipMate/FlipMate/Presentation/LoginScene/Flows/LoginFlowCoordinator.swift
@@ -0,0 +1,31 @@
+//
+//  LoginFlowCoordinator.swift
+//  FlipMate
+//
+//  Created by 임현규 on 2023/11/26.
+//
+
+import UIKit
+
+protocol LoginFlowCoordinatorDependencies {
+    func makeLoginViewController() -> LoginViewController
+}
+
+final class LoginFlowCoordinator {
+    private weak var navigationViewController: UINavigationController?
+    private let dependencies: LoginFlowCoordinatorDependencies
+    
+    private weak var loginViewController: LoginViewController?
+    
+    init(navigationViewController: UINavigationController? = nil, dependencies: LoginFlowCoordinatorDependencies) {
+        self.navigationViewController = navigationViewController
+        self.dependencies = dependencies
+    }
+    
+    func start() {
+        let viewController = dependencies.makeLoginViewController()
+        navigationViewController?.pushViewController(viewController, animated: true)
+        loginViewController = viewController
+    }
+}
+
