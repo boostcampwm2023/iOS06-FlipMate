e34e8a51423f04493c72168aac9760b1331f02d7
leemhyungyu <lhglbkksj@naver.com>
2023-11-26 03:28:11 +0900

    feat: TabBarFlowCoordinator 구현

---
+++ iOS/FlipMate/FlipMate/Presentation/TabBar/Flows/TabBarFlowCoordinator.swift
@@ -0,0 +1,63 @@
+//
+//  TabBarFlowCoordinator.swift
+//  FlipMate
+//
+//  Created by 임현규 on 2023/11/26.
+//
+
+import UIKit
+
+protocol TabBarFlowCoordinatorDependencies {
+    func makeTabBarController() -> UITabBarController
+    func makeTimerViewController() -> UIViewController
+    func makeTimerDIContainer() -> TimerSceneDIContainer
+}
+
+final class TabBarFlowCoordinator: Coordinator {
+    var childCoordinators: [Coordinator] = []
+    private var navigationController: UINavigationController
+    private weak var tabBarViewController: UITabBarController?
+    private let dependencies: TabBarFlowCoordinatorDependencies
+    
+    init(navigationController: UINavigationController, dependencies: TabBarFlowCoordinatorDependencies) {
+        self.navigationController = navigationController
+        self.dependencies = dependencies
+    }
+    
+    func start() {
+        let tabBarController = dependencies.makeTabBarController()
+        tabBarController.setViewControllers([makeSocialViewController(), makeTimerViewContorller(), makeChartViewController()], animated: true)
+        navigationController.view.window?.rootViewController = tabBarController
+        tabBarController.selectedIndex = 1
+        tabBarViewController = tabBarController
+    }
+    
+    private func makeTimerViewContorller() -> UINavigationController {
+        let navigationViewController = UINavigationController()
+        let timerSceneDIContainer = dependencies.makeTimerDIContainer()
+        let coordinator = timerSceneDIContainer.makeTimerFlowCoordinator(navigationController: navigationViewController)
+        coordinator.start()
+        return navigationViewController
+    }
+    
+    private func makeSocialViewController() -> UINavigationController {
+        let navigationViewController = UINavigationController()
+        navigationViewController.tabBarItem.image = UIImage(systemName: Constant.socialNomalImageName)
+        navigationViewController.tabBarItem.selectedImage = UIImage(systemName: Constant.socialSelectedImageName)
+        return navigationViewController
+    }
+
+    private func makeChartViewController() -> UINavigationController {
+        let navigationViewController = UINavigationController()
+        navigationViewController.tabBarItem.image = UIImage(systemName: Constant.chartNomalImageName)
+        navigationViewController.tabBarItem.selectedImage = UIImage(systemName: Constant.socialSelectedImageName)
+        return navigationViewController
+    }
+    
+    private enum Constant {
+        static let socialNomalImageName = "person.3"
+        static let socialSelectedImageName = "person.3.fill"
+        static let chartNomalImageName = "chart.bar"
+        static let chartSelectedImageName = "chart.bar.fill"
+    }
+}
