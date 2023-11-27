6ec3c9a4efb7eebf4e739bda801d9a28d478a4ec
nemanjabenkovic <evolroop@naver.com>
2023-11-27 20:26:36 +0900

    feat: HexString으로 UIColor 생성자 구현

--- iOS/FlipMate/FlipMate/Data/Extension/UIColor++Extension.swift
+++ iOS/FlipMate/FlipMate/Data/Extension/UIColor++Extension.swift
@@ -8,6 +8,22 @@
 import UIKit
 
 extension UIColor {
+    convenience init?(hexString: String) {
+        var hexSanitized = hexString.trimmingCharacters(in: .whitespacesAndNewlines)
+        var rgb: UInt64 = 0
+        
+        guard Scanner(string: hexSanitized).scanHexInt64(&rgb) else {
+            return nil
+        }
+        
+        let red = CGFloat((rgb & 0xFF000000) >> 24) / 255.0
+        let green = CGFloat((rgb & 0x00FF0000) >> 16) / 255.0
+        let blue = CGFloat((rgb & 0x0000FF00) >> 8) / 255.0
+        let alpha = CGFloat(rgb & 0x000000FF) / 255.0
+        
+        self.init(red: red, green: green, blue: blue, alpha: alpha)
+    }
+    
     func toHexString() -> String {
         guard let components = cgColor.components, components.count >= 3 else {
             return "FFFFFFFF"
