129c7f5f019457cccabbc9d7f430bad998646705
nemanjabenkovic <evolroop@naver.com>
2023-11-29 01:52:20 +0900

    feat: String, DateFormatter Extension 추가

---
+++ iOS/FlipMate/FlipMate/Presentation/SocialScene/View/SocialChartView.swift
@@ -0,0 +1,18 @@
+//
+//  SocialChartView.swift
+//  FlipMate
+//
+//  Created by 신민규 on 11/28/23.
+//
+
+import SwiftUI
+
+struct SocialChartView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SocialChartView()
+}
