3cc6c1cb9b9cccf46c42d911d4be89635a29d3f5
leemhyungyu <lhglbkksj@naver.com>
2023-11-29 19:50:02 +0900

    feat: 친구 검색, 팔로우 DTO 추가

---
+++ iOS/FlipMate/FlipMate/Data/Network/DataMapping/SocialScene/UserProfileResposeDTO.swift
@@ -0,0 +1,16 @@
+//
+//  UserProfileResposeDTO.swift
+//  FlipMate
+//
+//  Created by 임현규 on 2023/11/29.
+//
+
+import Foundation
+
+struct UserProfileResposeDTO: Decodable {
+    let profileImageURL: String
+    
+    private enum CodingKeys: String, CodingKey {
+        case profileImageURL = "image_url"
+    }
+}
