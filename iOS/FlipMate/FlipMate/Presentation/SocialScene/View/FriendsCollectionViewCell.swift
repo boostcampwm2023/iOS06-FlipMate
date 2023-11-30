c864850e674bc6a3e2b839ae1f4dd7b6ae733ed2
Eric Kwon / 권승용 <ericyong95@gmail.com>
2023-11-30 14:24:47 +0900

    [iOS#117] 소셜 뷰 UI 작성 (#266)
    * chore: 불필요한 토큰 삭제
    
    * feat: 소셜 뷰 컬렉션뷰 셀 작성
    
    - 친구들의 프로필 이미지와 이름, 상태를 보여주는 셀 작성
    
    * feat: 소셜 뷰 UI 작성
    
    - 마이페이지 및 친구 추가 버튼을 네비게이션 버튼 아이템에 구현
    - 내 정보를 상단에 보여주도록 구현
    - 친구들의 정보를 컬렉션뷰로 보여주도록 구현
    
    * chore: Podfile.lock 체크섬 업데이트
    
    * refactor: 상수 분리
    
    - 상수를 enum 타입으로 분리
    
    ---------
    
    Co-authored-by: 임현규 <48830320+leemhyungyu@users.noreply.github.com>

---
+++ iOS/FlipMate/FlipMate/Presentation/SocialScene/View/FriendsCollectionViewCell.swift
@@ -0,0 +1,117 @@
+//
+//  FriendsCollectionViewCell.swift
+//  FlipMate
+//
+//  Created by 권승용 on 11/29/23.
+//
+
+import UIKit
+
+final class FriendsCollectionViewCell: UICollectionViewCell {
+    static let identifier = "FriendsCollectionViewCell"
+    
+    private lazy var profileImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.contentMode = .scaleAspectFill
+        imageView.image = UIImage(resource: .group135)
+        imageView.clipsToBounds = true
+        imageView.bounds = CGRect(x: 0, y: 0, width: 90, height: 90)
+        imageView.layer.cornerRadius = imageView.bounds.height / 2
+        imageView.isUserInteractionEnabled = true
+        imageView.layer.borderWidth = 1
+        imageView.layer.borderColor = UIColor.red.cgColor
+        return imageView
+    }()
+    
+    private lazy var userNameLabel: UILabel = {
+        let label = UILabel()
+        label.font = FlipMateFont.mediumBold.font
+        label.text = Constant.defaultNickName
+        label.textColor = .label
+        label.textAlignment = .center
+        return label
+    }()
+    
+    private lazy var learningTimeLabel: UILabel = {
+        let label = UILabel()
+        label.font = FlipMateFont.mediumBold.font
+        label.text = Constant.defaultTime
+        label.textColor = .label
+        label.textAlignment = .center
+        return label
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configureUI()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("이 뷰는 스토리보드에서 사용할 수 없습니다")
+    }
+    
+    override func prepareForReuse() {
+        super.prepareForReuse()
+        FMLogger.appLifeCycle.log("prepare for reuse")
+    }
+    
+    private func configureUI() {
+        let subViews = [
+            profileImageView,
+            userNameLabel,
+            learningTimeLabel
+        ]
+        
+        subViews.forEach {
+            $0.translatesAutoresizingMaskIntoConstraints = false
+            self.addSubview($0)
+        }
+        
+        NSLayoutConstraint.activate([
+            profileImageView.topAnchor.constraint(equalTo: self.topAnchor),
+            profileImageView.widthAnchor.constraint(equalToConstant: 90),
+            profileImageView.heightAnchor.constraint(equalToConstant: 90),
+            profileImageView.centerXAnchor.constraint(equalTo: self.centerXAnchor),
+            
+            userNameLabel.topAnchor.constraint(equalTo: profileImageView.bottomAnchor, constant: 8),
+            userNameLabel.centerXAnchor.constraint(equalTo: self.centerXAnchor),
+            userNameLabel.leadingAnchor.constraint(equalTo: self.leadingAnchor),
+            userNameLabel.trailingAnchor.constraint(equalTo: self.trailingAnchor),
+            
+            learningTimeLabel.topAnchor.constraint(equalTo: userNameLabel.bottomAnchor, constant: 4),
+            learningTimeLabel.centerXAnchor.constraint(equalTo: self.centerXAnchor),
+            learningTimeLabel.leadingAnchor.constraint(equalTo: self.leadingAnchor),
+            learningTimeLabel.trailingAnchor.constraint(equalTo: self.trailingAnchor)
+        ])
+    }
+    
+    func configure(friend: FriendUser) {
+        var image: UIImage!
+        
+        if let url = URL(string: friend.profileImageURL), let data = try? Data(contentsOf: url) {
+            image = UIImage(data: data)
+        } else {
+            // url 오류 있으면 기본 이미지 보여주기
+            image = UIImage(resource: .group135)
+        }
+        
+        DispatchQueue.main.async {
+            self.profileImageView.image = image
+            self.userNameLabel.text = friend.name
+            self.learningTimeLabel.text = friend.time
+            self.profileImageView.layer.borderColor = friend.isOnline ? UIColor.green.cgColor : UIColor.red.cgColor
+        }
+    }
+}
+
+private extension FriendsCollectionViewCell {
+    enum Constant {
+        static let defaultNickName = "닉네임"
+        static let defaultTime = "00:00:00""
+    }
+}
+
+@available(iOS 17, *)
+#Preview {
+    FriendsCollectionViewCell()
+}
--- iOS/FlipMate/FlipMate/Presentation/SocialScene/ViewController/SocialViewController.swift
+++ iOS/FlipMate/FlipMate/Presentation/SocialScene/ViewController/SocialViewController.swift
@@ -7,9 +7,186 @@
 
 import UIKit
 
-final class SocialViewController: BaseViewController {
+enum Section: CaseIterable {
+    case main
+}
 
+struct FriendUser: Hashable {
+    let profileImageURL: String
+    let name: String
+    let time: String
+    var isOnline: Bool
+}
+
+final class SocialViewController: BaseViewController {
+    
+    // MARK: - View Properties
+    private lazy var profileImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.contentMode = .scaleAspectFill
+        imageView.image = UIImage(resource: .group135)
+        imageView.clipsToBounds = true
+        imageView.bounds = CGRect(x: 0, y: 0, width: 90, height: 90)
+        imageView.layer.cornerRadius = imageView.bounds.height / 2
+        imageView.isUserInteractionEnabled = true
+        return imageView
+    }()
+    
+    private lazy var userNameLabel: UILabel = {
+        let label = UILabel()
+        label.font = FlipMateFont.mediumBold.font
+        label.text = "닉네임"
+        label.textColor = .label
+        label.textAlignment = .center
+        return label
+    }()
+    
+    private lazy var learningTimeLabel: UILabel = {
+        let label = UILabel()
+        label.font = FlipMateFont.mediumBold.font
+        label.text = "00:00:00"
+        label.textColor = .label
+        label.textAlignment = .center
+        return label
+    }()
+    
+    private lazy var divider: UIView = {
+        let divider = UIView()
+        divider.backgroundColor = .gray
+        return divider
+    }()
+    
+    private lazy var friendsCollectionView: UICollectionView = {
+        let layout = UICollectionViewFlowLayout()
+        layout.sectionInset = UIEdgeInsets(top: 24, left: 16, bottom: 0, right: 16)
+        layout.minimumLineSpacing = 16
+        layout.minimumInteritemSpacing = 16
+        layout.itemSize = CGSize(width: UIScreen.main.bounds.width / 3.0 - 48, height: 150)
+        let collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        collectionView.register(FriendsCollectionViewCell.self)
+        collectionView.delegate = self
+        return collectionView
+    }()
+    
+    // MARK: - Properties
+    typealias DiffableDataSource = UICollectionViewDiffableDataSource<Section, FriendUser>
+    private var diffableDataSource: DiffableDataSource!
+    
+    // MARK: - View Life Cycle
     override func viewDidLoad() {
         super.viewDidLoad()
+        title = Constant.title
+        configureDiffableDataSource()
+        // TODO: 테스트를 위한 임시 데이터 지우고 뷰모델과 연동하기
+        let items = [
+            FriendUser(profileImageURL: "", name: "hid", time: "17:00", isOnline: true),
+            FriendUser(profileImageURL: "", name: "hie", time: "17:00", isOnline: true),
+            FriendUser(profileImageURL: "", name: "hif", time: "17:00", isOnline: true),
+            FriendUser(profileImageURL: "", name: "hix", time: "17:00", isOnline: true),
+            FriendUser(profileImageURL: "", name: "hiz", time: "17:00", isOnline: true),
+            FriendUser(profileImageURL: "", name: "hiy", time: "17:00", isOnline: true)
+        ]
+        var snapshot = NSDiffableDataSourceSnapshot<Section, FriendUser>()
+        snapshot.appendSections([.main])
+        snapshot.appendItems(items)
+        self.diffableDataSource.apply(snapshot, animatingDifferences: true)
     }
+        
+    // MARK: - Configure UI
+    override func configureUI() {
+        let subviews = [
+            profileImageView,
+            userNameLabel,
+            learningTimeLabel,
+            divider,
+            friendsCollectionView
+        ]
+        
+        subviews.forEach {
+            view.addSubview($0)
+            $0.translatesAutoresizingMaskIntoConstraints = false
+        }
+        
+        NSLayoutConstraint.activate([
+            profileImageView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 32),
+            profileImageView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            profileImageView.widthAnchor.constraint(equalToConstant: 90),
+            profileImageView.heightAnchor.constraint(equalToConstant: 90),
+            
+            userNameLabel.topAnchor.constraint(equalTo: profileImageView.bottomAnchor, constant: 8),
+            userNameLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            
+            learningTimeLabel.topAnchor.constraint(equalTo: userNameLabel.bottomAnchor, constant: 4),
+            learningTimeLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            
+            divider.topAnchor.constraint(equalTo: learningTimeLabel.bottomAnchor, constant: 24),
+            divider.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor),
+            divider.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor),
+            divider.heightAnchor.constraint(equalToConstant: 1),
+            
+            friendsCollectionView.topAnchor.constraint(equalTo: divider.bottomAnchor),
+            friendsCollectionView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor),
+            friendsCollectionView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor),
+            friendsCollectionView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor)
+        ])
+    }
+    
+    private func configureDiffableDataSource() {
+        diffableDataSource = DiffableDataSource(collectionView: friendsCollectionView) { collectionView, indexPath, friend in
+            guard let cell = collectionView.dequeueReusableCell(
+                withReuseIdentifier: FriendsCollectionViewCell.identifier,
+                for: indexPath) as? FriendsCollectionViewCell else {
+                preconditionFailure()
+            }
+            cell.configure(friend: friend)
+            return cell
+        }
+    }
+    
+    private func configureNavigationBarItems() {
+        let myPageButton = UIBarButtonItem(
+            image: UIImage(systemName: "person"),
+            style: .plain,
+            target: self,
+            action: #selector(myPageButtonTapped))
+        let addFriendButton = UIBarButtonItem(
+            image: UIImage(systemName: "person.badge.plus"),
+            style: .plain,
+            target: self,
+            action: #selector(addFriendButtonTapped))
+        myPageButton.tintColor = .label
+        addFriendButton.tintColor = .label
+        navigationItem.leftBarButtonItem = myPageButton
+        navigationItem.rightBarButtonItem = addFriendButton
+    }
+}
+
+// MARK: - Selector methods
+private extension SocialViewController {
+    @objc
+    func myPageButtonTapped() {
+        
+    }
+    
+    @objc
+    func addFriendButtonTapped() {
+        
+    }
+}
+
+// MARK: - UICollectionViewDelegate
+extension SocialViewController: UICollectionViewDelegate {
+    
+}
+
+// MARK: - Constants
+private extension SocialViewController {
+    enum Constant {
+        static let title = "소셜"
+    }
+}
+
+@available(iOS 17, *)
+#Preview {
+    SocialViewController()
 }
