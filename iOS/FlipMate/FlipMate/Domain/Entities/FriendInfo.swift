6cd01412d59385f56669d27d2bd873602fca685b
nemanjabenkovic <evolroop@naver.com>
2023-11-30 18:20:00 +0900

    feat: 구조체 생성

---
+++ iOS/FlipMate/FlipMate/Domain/Entities/FriendInfo.swift
@@ -0,0 +1,14 @@
+//
+//  FriendInfo.swift
+//  FlipMate
+//
+//  Created by 신민규 on 11/30/23.
+//
+
+import Foundation
+
+struct FriendInfo {
+    let friend: Friend
+    let id: Int
+    let totalTime: Int
+}
---
+++ iOS/FlipMate/FlipMate/Domain/Entities/SocialChart.swift
@@ -0,0 +1,15 @@
+//
+//  SocialChart.swift
+//  FlipMate
+//
+//  Created by 신민규 on 11/30/23.
+//
+
+import Foundation
+
+struct SocialChart {
+    let myData: [Int]
+    let friendData: [Int]
+    let primaryCategory: String?
+}
+
