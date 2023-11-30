adda77bb8f0bbb6d40ca17a52c65dc68cf317d58
leemhyungyu <lhglbkksj@naver.com>
2023-11-30 23:16:13 +0900

    feat: 받아온 Entity로 ViewModel과 CollectionView 바인딩

--- iOS/FlipMate/FlipMate/Presentation/SocialScene/ViewModel/SocialViewModel.swift
+++ iOS/FlipMate/FlipMate/Presentation/SocialScene/ViewModel/SocialViewModel.swift
@@ -13,24 +13,56 @@ struct SocialViewModelActions {
 }
 
 protocol SocialViewModelInput {
+    func viewWillAppear()
     func freindAddButtonDidTapped()
 }
 
 protocol SocialViewModelOutput {
-    
+    var freindsPublisher: AnyPublisher<[Friend], Never> { get }
 }
 
 typealias SocialViewModelProtocol = SocialViewModelInput & SocialViewModelOutput
 
 final class SocialViewModel: SocialViewModelProtocol {
+    // MARK: - Subject
+    private var freindsSubject = PassthroughSubject<[Friend], Never>()
     
+    // MARK: - Properties
+    private var cancellables = Set<AnyCancellable>()
     private let actions: SocialViewModelActions?
+    private let socialUseCase: SocialUseCase
     
-    init(actions: SocialViewModelActions? = nil) {
+    // MARK: - init
+    init(actions: SocialViewModelActions? = nil, socialUseCase: SocialUseCase) {
         self.actions = actions
+        self.socialUseCase = socialUseCase
     }
     
+    // MARK: - Output
+    var freindsPublisher: AnyPublisher<[Friend], Never> {
+        return freindsSubject.eraseToAnyPublisher()
+    }
+    
+    // MARK: - Input
     func freindAddButtonDidTapped() {
         actions?.showFriendAddViewController()
+    }
+    
+    func viewWillAppear() {
+        socialUseCase.getMyFriend(date: Date())
+            .receive(on: DispatchQueue.main)
+            .sink { completion in
+                switch completion {
+                case .finished:
+                    FMLogger.friend.debug("친구 받아오기 성공")
+                case .failure(let error):
+                    FMLogger.friend.error("친구 받아오기 에러 발생 \(error)")
+                }
+            } receiveValue: { [weak self] freinds in
+                guard let self = self else { return }
+                self.freindsSubject.send(freinds)
+            }
+            .store(in: &cancellables)
+
     }
 }
