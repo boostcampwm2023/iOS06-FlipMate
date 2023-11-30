f08713e8bb87b3b4699630d160143ade2b1aa142
ericKwon95 <ericyong95@gmail.com>
2023-11-30 18:57:12 +0900

    feat: 임시 로그아웃 버튼 생성 및 연결

---
+++ iOS/FlipMate/FlipMate/Presentation/MyPageScene/MyPageViewController.swift
@@ -0,0 +1,40 @@
+//
+//  MyPageViewController.swift
+//  FlipMate
+//
+//  Created by 권승용 on 11/30/23.
+//
+
+import UIKit
+
+final class MyPageViewController: BaseViewController {
+    private lazy var logoutButton: UIButton = {
+        let button = UIButton()
+        button.setTitle("로그아웃", for: .normal)
+        button.titleLabel?.font = FlipMateFont.largeBold.font
+        button.addTarget(self, action: #selector(logoutButtonTapped), for: .touchUpInside)
+        return button
+    }()
+    
+    override func configureUI() {
+        let subviews = [
+            logoutButton
+        ]
+        
+        subviews.forEach {
+            view.addSubview($0)
+            $0.translatesAutoresizingMaskIntoConstraints = false
+        }
+        
+        NSLayoutConstraint.activate([
+            logoutButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            logoutButton.centerYAnchor.constraint(equalTo: view.centerYAnchor)
+        ])
+    }
+    
+    @objc
+    func logoutButtonTapped() {
+        _ = try? KeychainManager.deleteAccessToken()
+        exit(0)
+    }
+}
