d34185a7b36831a0ac514748f45dd021da165002
leemhyungyu <lhglbkksj@naver.com>
2023-11-30 23:11:12 +0900

    feat: SocialUseCase 구현

---
+++ iOS/FlipMate/FlipMate/Domain/UseCases/DefaultSocialUseCase.swift
@@ -0,0 +1,21 @@
+//
+//  DefaultSocialUseCase.swift
+//  FlipMate
+//
+//  Created by 임현규 on 2023/11/30.
+//
+
+import Foundation
+import Combine
+
+final class DefaultSocialUseCase: SocialUseCase {
+    private let repsoitory: SocialRepository
+    
+    init(repsoitory: SocialRepository) {
+        self.repsoitory = repsoitory
+    }
+    
+    func getMyFriend(date: Date) -> AnyPublisher<[Friend], NetworkError> {
+        return repsoitory.getMyFriend(date: date)
+    }
+}
