94329c187b8e8ae4ce97343b6203adbb6417f78a
nemanjabenkovic <evolroop@naver.com>
2023-12-04 01:52:35 +0900

    feat: EndPoint 추가

--- iOS/FlipMate/FlipMate/Data/Network/FriendEndpoints.swift
+++ iOS/FlipMate/FlipMate/Data/Network/FriendEndpoints.swift
@@ -18,10 +18,23 @@ struct FriendEndpoints {
             data: data)
     }
     
+    static func unfollowFreind(with friendUnfollowRequestDTO: FriendUnfollowRequestDTO) -> EndPoint<StatusResponseDTO> {
+        let encoder = JSONEncoder()
+        let data = try? encoder.encode(friendUnfollowRequestDTO)
+        return EndPoint (baseURL: BaseURL.flipmateDomain, path: Paths.friend, method: .delete, data: data)
+    }
+    
     static func searchFriend(at nickname: String) -> EndPoint<UserProfileResposeDTO> {
         return EndPoint(
             baseURL: BaseURL.flipmateDomain,
             path: Paths.user + "/profile?nickname=\(nickname)",
             method: .get)
+    }
+    
+    static func loadFriendData(with socialDetailRequestDTO: SocialDetailRequestDTO) -> EndPoint<SocialDetailResponseDTO> {
+        let encoder = JSONEncoder()
+        let data = try? encoder.encode(socialDetailRequestDTO)
+        let path = Paths.friend + "/\(socialDetailRequestDTO.followingID)" + "/stats"
+        return EndPoint(baseURL: BaseURL.flipmateDomain, path: path, method: .get, data: data)
     }
 }
