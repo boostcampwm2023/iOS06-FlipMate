7cd8af7a997bbeac9932c49c08355bdce2c03bc5
nemanjabenkovic <evolroop@naver.com>
2023-12-04 02:00:24 +0900

    feat: Repository 생성

--- iOS/FlipMate/FlipMate/Data/Repositories/DefaultFriendRepository.swift
+++ iOS/FlipMate/FlipMate/Data/Repositories/DefaultFriendRepository.swift
@@ -26,11 +26,31 @@ final class DefaultFriendRepository: FriendRepository {
             .eraseToAnyPublisher()
     }
     
+    func unfollow(at id: Int) -> AnyPublisher<String, NetworkError> {
+        let requestDTO = FriendUnfollowRequestDTO(id: id)
+        let endPoint = FriendEndpoints.unfollowFreind(with: requestDTO)
+        return provider.request(with: endPoint)
+            .map { response -> String in
+                return response.message
+            }
+            .eraseToAnyPublisher()
+    }
+    
     func search(at nickname: String) -> AnyPublisher<String?, NetworkError> {
         let endPoint = FriendEndpoints.searchFriend(at: nickname)
         return provider.request(with: endPoint)
             .map { response -> String? in
                 return response.profileImageURL
+            }
+            .eraseToAnyPublisher()
+    }
+    
+    func loadChart(at id: Int) -> AnyPublisher<SocialChart, NetworkError> {
+        let requestDTO = SocialDetailRequestDTO(followingID: id, date: Date().dateToString(format: .yyyyMMdd))
+        let endPoint = FriendEndpoints.loadFriendData(with: requestDTO)
+        return provider.request(with: endPoint)
+            .map { response -> SocialChart in
+                return SocialChart(myData: response.myData, friendData: response.followingData, primaryCategory: response.primaryCategory)
             }
             .eraseToAnyPublisher()
     }
