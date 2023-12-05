c1e96902ae1a8325ce2fbf7275fcfe615ca749f6
ericKwon95 <ericyong95@gmail.com>
2023-12-05 21:56:04 +0900

    feat: 프로필 설정 뷰모델 작성
    - 프로필 설정 및 저장 기능 구현

---
+++ iOS/FlipMate/FlipMate/Presentation/MyPageScene/ViewModel/ProfileSettingsViewModel.swift
@@ -0,0 +1,126 @@
+//
+//  ProfileSettingsViewModel.swift
+//  FlipMate
+//
+//  Created by 권승용 on 12/5/23.
+//
+
+import Foundation
+import Combine
+
+struct ProfileSettingsViewModelActions {
+    let didFinishSignUp: () -> Void
+}
+
+protocol ProfileSettingsViewModelInput {
+    func viewReady()
+    func nickNameChanged(_ newNickName: String)
+    func profileImageChanged()
+    func signUpButtonTapped(userName: String, profileImageData: Data)
+}
+
+protocol ProfileSettingsViewModelOutput {
+    var nicknamePublisher: AnyPublisher<String, Never> { get }
+    var imageDataPublisher: AnyPublisher<Data, Never> { get }
+    var isValidNickNamePublisher: AnyPublisher<NickNameValidationState, Never> { get }
+    var isProfileImageChangedPublisher: AnyPublisher<Void, Never> { get }
+    var isSignUpCompletedPublisher: AnyPublisher<Void, Never> { get }
+    var errorPublisher: AnyPublisher<Error, Never> { get }
+}
+
+typealias ProfileSettingsViewModelProtocol = ProfileSettingsViewModelInput & ProfileSettingsViewModelOutput
+
+final class ProfileSettingsViewModel: ProfileSettingsViewModelProtocol {
+    // MARK: - Use Case
+    private let useCase: ProfileSettingsUseCase
+    
+    // MARK: - Subjects
+    private var nameSubject = PassthroughSubject<String, Never>()
+    private var imageDataSubject = PassthroughSubject<Data, Never>()
+    private var isValidNickNameSubject = PassthroughSubject<NickNameValidationState, Never>()
+    private var isProfileImageChangedSubject = PassthroughSubject<Void, Never>()
+    private var isSignUpCompletedSubject = PassthroughSubject<Void, Never>()
+    private var errorSubject = PassthroughSubject<Error, Never>()
+    private let actions: ProfileSettingsViewModelActions?
+    
+    init(usecase: ProfileSettingsUseCase, actions: ProfileSettingsViewModelActions) {
+        self.useCase = usecase
+        self.actions = actions
+    }
+    
+    // MARK: - Input
+    func viewReady() {
+        let nickname = UserInfoStorage.nickname
+        nameSubject.send(nickname)
+        guard let url = URL(string: UserInfoStorage.profileImageURL) else {
+            return
+        }
+        DispatchQueue.global().async {
+            do {
+                let data = try Data(contentsOf: url)
+                self.imageDataSubject.send(data)
+            } catch let error {
+                self.errorSubject.send(error)
+            }
+        }
+    }
+    
+    func nickNameChanged(_ newNickName: String) {
+        Task {
+            do {
+                let nickNameValidationStatus = try await useCase.isNickNameValid(newNickName)
+                isValidNickNameSubject.send(nickNameValidationStatus)
+            } catch let error {
+                errorSubject.send(error)
+            }
+        }
+    }
+    
+    func profileImageChanged() {
+        isProfileImageChangedSubject.send()
+    }
+    
+    func signUpButtonTapped(userName: String, profileImageData: Data) {
+        Task {
+            do {
+                let userInfo = try await useCase.setupProfileInfo(nickName: userName, profileImageData: profileImageData)
+                isSignUpCompletedSubject.send()
+                UserInfoStorage.nickname = userInfo.name
+                UserInfoStorage.profileImageURL = userInfo.profileImageURL ?? ""
+                DispatchQueue.main.async {
+                    self.actions?.didFinishSignUp()
+                }
+            } catch let error {
+                errorSubject.send(error)
+            }
+        }
+    }
+    
+    // MARK: - Output
+    var nicknamePublisher: AnyPublisher<String, Never> {
+        return nameSubject.eraseToAnyPublisher()
+    }
+    
+    var imageDataPublisher: AnyPublisher<Data, Never> {
+        return imageDataSubject.eraseToAnyPublisher()
+    }
+    var isValidNickNamePublisher: AnyPublisher<NickNameValidationState, Never> {
+        return isValidNickNameSubject
+            .eraseToAnyPublisher()
+    }
+    
+    var isProfileImageChangedPublisher: AnyPublisher<Void, Never> {
+        return isProfileImageChangedSubject
+            .eraseToAnyPublisher()
+    }
+    
+    var isSignUpCompletedPublisher: AnyPublisher<Void, Never> {
+        return isSignUpCompletedSubject
+            .eraseToAnyPublisher()
+    }
+    
+    var errorPublisher: AnyPublisher<Error, Never> {
+        return errorSubject
+            .eraseToAnyPublisher()
+    }
+}
