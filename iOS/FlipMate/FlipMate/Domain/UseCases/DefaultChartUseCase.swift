d1d53ad90744b852bb42a2daeaf587e9f56bd7ce
nemanjabenkovic <evolroop@naver.com>
2023-12-06 00:38:27 +0900

    feat: 기타 카테고리 계산 로직 추가

--- iOS/FlipMate/FlipMate/Domain/UseCases/DefaultChartUseCase.swift
+++ iOS/FlipMate/FlipMate/Domain/UseCases/DefaultChartUseCase.swift
@@ -20,6 +20,17 @@ final class DefaultChartUseCase: ChartUseCase {
     }
     
     func fetchDailyChartLog(at date: Date) async throws -> ChartLog {
-        return try await repository.fetchDailyLog(date: date)
+        var chartLog = try await repository.fetchDailyLog(date: date)
+        
+        var etcTime = chartLog.studyLog.category.reduce(0) { (result, category) in
+            return result + (category.studyTime ?? 0)
+        }
+        
+        etcTime = chartLog.studyLog.totalTime - etcTime
+        
+        let etcCategory = Category(id: 0, color: "FFFFFFFF", subject: "기타", studyTime: etcTime)
+            chartLog.studyLog.category.append(etcCategory)
+        
+        return chartLog
     }
 }
