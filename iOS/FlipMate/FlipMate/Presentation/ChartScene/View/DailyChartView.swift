da2722b27cc72504b14c73311df8ecbcb7a3063d
nemanjabenkovic <evolroop@naver.com>
2023-12-06 17:36:54 +0900

    refactor: 일일 차트 스크롤 기능 추가

--- iOS/FlipMate/FlipMate/Presentation/ChartScene/View/DailyChartView.swift
+++ iOS/FlipMate/FlipMate/Presentation/ChartScene/View/DailyChartView.swift
@@ -19,33 +19,34 @@ struct DailyChartView: View {
     var body: some View {
         VStack {
             CustomCalenderView(selectedDate: $selectedDate, viewModel: viewModel)
-            if #available(iOS 17.0, *) {
-                Chart {
-                    ForEach(viewModel.dailyChartLog.studyLog.category, id: \.subject) { category in
-                        SectorMark(angle: .value("시간", category.studyTime ?? 0), innerRadius: .ratio(0.65), angularInset: 3.0)
-                            .foregroundStyle(by: .value("카테고리", category.subject))
-                            .cornerRadius(10.0)
-                            .annotation(position: .overlay) {
-                                if category.studyTime != 0 {
-                                    Text("\(category.studyTime ?? 0)")
-                                        .font(.headline)
+            ScrollView(.vertical) {
+                if #available(iOS 17.0, *) {
+                    Chart {
+                        ForEach(viewModel.dailyChartLog.studyLog.category, id: \.subject) { category in
+                            SectorMark(angle: .value("시간", category.studyTime ?? 0), innerRadius: .ratio(0.65), angularInset: 3.0)
+                                .foregroundStyle(by: .value("카테고리", category.subject))
+                                .cornerRadius(10.0)
+                                .annotation(position: .overlay) {
+                                    if category.studyTime != 0 {
+                                        Text("\(category.studyTime ?? 0)")
+                                            .font(.headline)
+                                    }
                                 }
-                            }
+                        }
                     }
-                }
-                .frame(height: 400 * (UIScreen.main.bounds.size.height / 844))
-                .chartBackground { _ in
-                    VStack {
-                        Text("총 학습 시간").font(.callout)
-                            .foregroundStyle(.secondary)
-                        Text("\(viewModel.dailyChartLog.studyLog.totalTime.secondsToStringTime())").font(.system(size: 36))}
-                    .padding(.bottom, 20)
-                }
-            } else if #available(iOS 16.0, *) {
+                    .frame(height: 400 * (UIScreen.main.bounds.size.height / 844))
+                    .chartBackground { _ in
+                        VStack {
+                            Text("총 학습 시간").font(.callout)
+                                .foregroundStyle(.secondary)
+                            Text("\(viewModel.dailyChartLog.studyLog.totalTime.secondsToStringTime())").font(.system(size: 36))}
+                        .padding(.bottom, 20)
+                    }
+                } else if #available(iOS 16.0, *) {
                     Text("총 학습 시간").font(.callout)
                         .foregroundStyle(.secondary)
                     Text("\(viewModel.dailyChartLog.studyLog.totalTime.secondsToStringTime())").font(.system(size: 36))
-        
+                    
                     Chart {
                         ForEach(viewModel.dailyChartLog.studyLog.category, id: \.subject) { category in
                             BarMark(x: .value("시간", Float(category.studyTime ?? 0) / 60), y: .value("카테고리", category.subject))
@@ -55,8 +56,9 @@ struct DailyChartView: View {
                     .chartLegend(.hidden)
                     .frame(height: 360 * (UIScreen.main.bounds.size.height / 844))
                     .chartXAxisLabel("분 (m)")
-            } else {
-                Text("iOS 16.0 이상 버전부터 차트 기능 사용 가능")
+                } else {
+                    Text("iOS 16.0 이상 버전부터 차트 기능 사용 가능")
+                }
             }
         }.padding()
     }
