c1a09c56d87e25619d1c0d4a264fe74fbcdb0e65
victolee0 <39608452+victolee0@users.noreply.github.com>
2023-12-06 23:18:05 +0900

    test: 기본 모듈 세팅

--- BE/src/users/users.service.spec.ts
+++ BE/src/users/users.service.spec.ts
@@ -1,12 +1,28 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UsersService } from './users.service';
+import { S3Service } from 'src/common/s3.service';
+import { MockConfigService } from '../../test/mock-service/mock-config-service';
+import { ConfigService } from '@nestjs/config';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { MockUsersRepository } from '../../test/mock-repo/mock-user-repo';
+import { UsersModel } from './entity/users.entity';
+
+class MockS3Service {}
 
 describe('UsersService', () => {
   let service: UsersService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [UsersService],
+      providers: [
+        UsersService,
+        {
+          provide: getRepositoryToken(UsersModel),
+          useClass: MockUsersRepository,
+        },
+        { provide: S3Service, useClass: MockS3Service },
+        { provide: ConfigService, useClass: MockConfigService },
+      ],
     }).compile();
 
     service = module.get<UsersService>(UsersService);
