98ddb45354ce872778733660559ecead11124e5d
nemanjabenkovic <evolroop@naver.com>
2023-12-13 23:30:26 +0900

    feat: DailyChart category 선택 기능

--- iOS/FlipMate/FlipMate/Presentation/ChartScene/View/DailyChartView.swift
+++ iOS/FlipMate/FlipMate/Presentation/ChartScene/View/DailyChartView.swift
@@ -12,7 +12,7 @@ struct DailyChartView: View {
     @ObservedObject var viewModel: ChartViewModel
     @State private var selectedDate = Date()
     @State private var selectedCount: Int?
-    @State private var selectedSector: String?
+    @State private var selectedSector: Category?
     @Environment(\.colorScheme) var colorScheme
     
     init(viewModel: ChartViewModel) {
@@ -20,35 +20,46 @@ struct DailyChartView: View {
     }
     
     var body: some View {
+        let studyLogs = viewModel.dailyChartLog.studyLog
+        let categorySet = studyLogs.category
+        let totalTimes = studyLogs.totalTime
         ScrollView(.vertical) {
             VStack {
                 CustomCalenderView(selectedDate: $selectedDate, viewModel: viewModel)
                 if #available(iOS 17.0, *) {
                     Chart {
-                        ForEach(viewModel.dailyChartLog.studyLog.category, id: \.subject) { category in
+                        ForEach(categorySet, id: \.subject) { category in
                             SectorMark(angle: .value(Constant.time, category.studyTime ?? 0), innerRadius: .ratio(0.65), angularInset: 3.0)
                                 .foregroundStyle(by: .value(Constant.category, category.subject))
                                 .cornerRadius(10.0)
                                 .annotation(position: .overlay) {
-                                    if getRatio(time: category.studyTime ?? 0, sum: viewModel.dailyChartLog.studyLog.totalTime) > 0.05 {
+                                    if getRatio(time: category.studyTime ?? 0, sum: totalTimes) > 0.05 {
                                         DailyChartView.StrokeText(text: "\(category.studyTime ?? 0)",
                                                                   width: 0.3, color: colorScheme == .dark ? .black : .white)
                                         .font(.headline)
                                     }
                                 }
-                                .opacity(selectedSector == nil ? 1.0 : (selectedSector == category.subject ? 1.0 : 0.3))
+                                .opacity(selectedSector == nil ? 1.0 : (selectedSector == category ? 1.0 : 0.3))
                         }
                     }
-                    .chartForegroundStyleScale(domain: viewModel.dailyChartLog.studyLog.category.compactMap({ category in
+                    .chartForegroundStyleScale(domain: categorySet.compactMap({ category in
                         category.subject
-                    }), range: getColorArray(categories: viewModel.dailyChartLog.studyLog.category))
+                    }), range: getColorArray(categories: categorySet))
                     .frame(height: 400 * (UIScreen.main.bounds.size.height / 844))
                     .chartBackground { _ in
-                        VStack {
-                            Text(Constant.totalTime).font(.callout)
-                                .foregroundStyle(.secondary)
-                            Text("\(viewModel.dailyChartLog.studyLog.totalTime.secondsToStringTime())").font(.system(size: 36))}
-                        .padding(.bottom, 20)
+                        if selectedSector == nil {
+                            VStack {
+                                Text(Constant.totalTime).font(.callout)
+                                    .foregroundStyle(.secondary)
+                                Text("\(totalTimes.secondsToStringTime())").font(.system(size: 36))}
+                            .padding(.bottom, 20)
+                        } else {
+                            VStack {
+                                Text("\(selectedSector?.subject ?? "")").font(.callout)
+                                    .foregroundStyle(.primary)
+                                Text("\((selectedSector?.studyTime ?? 0).secondsToStringTime())").font(.system(size: 36))}
+                            .padding(.bottom, 20)
+                        }
                     }
                     .chartAngleSelection(value: $selectedCount)
                     .onChange(of: selectedCount) { _, newValue in
@@ -61,19 +72,19 @@ struct DailyChartView: View {
                 } else if #available(iOS 16.0, *) {
                     Text(Constant.totalTime).font(.callout)
                         .foregroundStyle(.secondary)
-                    Text("\(viewModel.dailyChartLog.studyLog.totalTime.secondsToStringTime())").font(.system(size: 36))
+                    Text("\(totalTimes.secondsToStringTime())").font(.system(size: 36))
                     
                     Chart {
-                        ForEach(viewModel.dailyChartLog.studyLog.category, id: \.subject) { category in
+                        ForEach(categorySet, id: \.subject) { category in
                             BarMark(x: .value(Constant.time, Float(category.studyTime ?? 0) / 60), y: .value(Constant.category, category.subject))
                                 .foregroundStyle(by: .value("", category.color))
                         }
                     }
                     .chartLegend(.hidden)
-                      .chartForegroundStyleScale(domain: viewModel.dailyChartLog.studyLog.category.compactMap({ category in
+                      .chartForegroundStyleScale(domain: categorySet.compactMap({ category in
                         category.subject
-                    }), range: getColorArray(categories: viewModel.dailyChartLog.studyLog.category))
-                    .frame(height: CGFloat(60 * viewModel.dailyChartLog.studyLog.category.count) * (UIScreen.main.bounds.size.height / 844))
+                    }), range: getColorArray(categories: categorySet))
+                    .frame(height: CGFloat(60 * categorySet.count) * (UIScreen.main.bounds.size.height / 844))
                     .chartXAxisLabel(Constant.min)
                 } else {
                     Text(Constant.message)
@@ -97,7 +108,7 @@ struct DailyChartView: View {
         return Float(time) / Float(sum)
     }
     
-    private func findSelectorSector(value: Int) -> String? {
+    private func findSelectorSector(value: Int) -> Category? {
         var accumulatedCount = 0
         
         let category = viewModel.dailyChartLog.studyLog.category.first { category in
@@ -105,7 +116,7 @@ struct DailyChartView: View {
             return value <= accumulatedCount
         }
         
-        return category?.subject
+        return category
     }
 }
 
