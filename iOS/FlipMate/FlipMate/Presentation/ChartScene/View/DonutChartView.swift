8c9a3d8eb3141f07177c1a4d70b21fb29f8155c2
leemhyungyu <lhglbkksj@naver.com>
2024-01-06 21:33:37 +0900

    feat: DonutChartView UIView 생성

---
+++ iOS/FlipMate/FlipMate/Presentation/ChartScene/View/DonutChartView.swift
@@ -0,0 +1,19 @@
+//
+//  DonutChartView.swift
+//  FlipMate
+//
+//  Created by 임현규 on 2024/01/06.
+//
+
+import UIKit
+
+final class DonutChartView: UIView {
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("Don't use storyBoard")
+    }
+}
