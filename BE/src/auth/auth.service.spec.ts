c14c88b08cc702411de3a2b528b907153501b2c3
victolee0 <39608452+victolee0@users.noreply.github.com>
2024-01-06 19:03:46 +0900

    test: 테스트 코드 수정
    - user 서비스 테스트 코드 수정
    - 컨트롤러 테스트 코드 삭제

--- BE/src/auth/auth.service.spec.ts
+++ BE/src/auth/auth.service.spec.ts
@@ -12,7 +12,7 @@ describe('AuthService', () => {
     service = module.get<AuthService>(AuthService);
   });
 
-  it('should be defined', () => {
+  it.todo('should be defined', () => {
     expect(service).toBeDefined();
   });
 });
