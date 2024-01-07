a5475ef38ea9bc5a8a681083300e152c4301f40d
victolee0 <39608452+victolee0@users.noreply.github.com>
2024-01-07 16:12:58 +0900

    test: auth service 모듈 임포트

--- BE/src/auth/auth.service.spec.ts
+++ BE/src/auth/auth.service.spec.ts
@@ -1,18 +1,31 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AuthService } from './auth.service';
+import { JwtService } from '@nestjs/jwt';
+import { UsersService } from 'src/users/users.service';
+import { MockUsersService } from '../../test/mock-service/mock-user-service';
 
 describe('AuthService', () => {
   let service: AuthService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [AuthService],
+      providers: [
+        AuthService,
+        {
+          provide: JwtService,
+          useValue: {},
+        },
+        {
+          provide: UsersService,
+          useClass: MockUsersService,
+        },
+      ],
     }).compile();
 
     service = module.get<AuthService>(AuthService);
   });
 
-  it.todo('should be defined', () => {
+  it('should be defined', () => {
     expect(service).toBeDefined();
   });
 });
