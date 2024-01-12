be891f4f531107f3401cc6974d4e509c64ec9153
ericKwon95 <ericyong95@gmail.com>
2024-01-12 15:28:46 +0900

    feat: 로그인 뷰모델 테스트코드 작성

---
+++ iOS/FlipMate/FlipMateTests/FlipMateViewModelTests/LoginScene/LoginViewModelTests.swift
@@ -0,0 +1,129 @@
+//
+//  LoginViewModel.swift
+//  FlipMateTests
+//
+//  Created by 권승용 on 1/8/24.
+//
+
+import Foundation
+import XCTest
+import Combine
+@testable import FlipMate
+
+private enum TestToken {
+    static let success = "success"
+    static let fail = "fail"
+}
+
+private enum LoginError: Error {
+    case loginError
+}
+
+private final class GoogleLoginUseCaseStub: GoogleLoginUseCase {
+    func googleLogin(accessToken: String) async throws -> User {
+        if accessToken == TestToken.success {
+            return User(isMember: true, accessToken: accessToken)
+        } else {
+            throw LoginError.loginError
+        }
+    }
+}
+
+private final class AppleLoginUseCaseStub: AppleLoginUseCase {
+    func appleLogin(accessToken: String, userID: String) async throws -> User {
+        if accessToken == TestToken.success {
+            return User(isMember: true, accessToken: accessToken)
+        } else {
+            throw LoginError.loginError
+        }
+    }
+}
+
+final class LoginViewModelTests: XCTestCase {
+    private var sut: LoginViewModelProtocol!
+ 
+    override func setUpWithError() throws {
+        let googleLoginUseCase = GoogleLoginUseCaseStub()
+        let appleLoginUseCase = AppleLoginUseCaseStub()
+        sut = LoginViewModel(googleLoginUseCase: googleLoginUseCase, appleLoginUseCase: appleLoginUseCase)
+    }
+    
+    override func tearDownWithError() throws {
+        sut = nil
+    }
+    
+    func test_구글로그인_성공_true_반환() throws {
+        // arrange
+        let expectation = XCTestExpectation()
+        var received: Bool!
+        let cancellable = sut.isMemberPublisher
+            .sink { isMember in
+                received = isMember
+                XCTAssertEqual(received, true)
+                expectation.fulfill()
+            }
+        
+        // act
+        sut.requestGoogleLogin(accessToken: TestToken.success)
+        wait(for: [expectation], timeout: 10)
+        
+        cancellable.cancel()
+    }
+    
+    func test_구글로그인_실패_error_throw() throws {
+        // arrange
+        let expectation = XCTestExpectation()
+        var received: LoginError!
+        let cancellable = sut.errorPublisher
+            .sink { error in
+                received = error as? LoginError
+                expectation.fulfill()
+            }
+        
+        // act
+        sut.requestGoogleLogin(accessToken: TestToken.fail)
+        wait(for: [expectation], timeout: 10)
+        
+        // assert
+        XCTAssertEqual(received, LoginError.loginError)
+        cancellable.cancel()
+    }
+    
+    func test_애플로그인_성공_true_반환() {
+        // arrange
+        let expectation = XCTestExpectation()
+        var received: Bool!
+        let cancellable = sut.isMemberPublisher
+            .sink { isMember in
+                received = isMember
+                expectation.fulfill()
+            }
+        
+        // act
+        sut.requestAppleLogin(accessToken: TestToken.success, userID: TestToken.success)
+        wait(for: [expectation], timeout: 10)
+        
+        // assert
+        XCTAssertEqual(received, true)
+        cancellable.cancel()
+    }
+    
+    func test_애플로그인_실패_error_throw() {
+        // arrange
+        let expectation = XCTestExpectation()
+        var received: LoginError!
+        let cancellable = sut.errorPublisher
+            .sink { error in
+                received = error as? LoginError
+                expectation.fulfill()
+            }
+        
+        // act
+        sut.requestAppleLogin(accessToken: TestToken.fail, userID: TestToken.fail)
+        wait(for: [expectation], timeout: 10)
+        
+        // assert
+        XCTAssertEqual(received, LoginError.loginError)
+        cancellable.cancel()
+    }
+}
