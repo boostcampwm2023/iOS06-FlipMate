263c4d7d5995f82a309897fd563a55433b76c007
nemanjabenkovic <evolroop@naver.com>
2024-01-16 19:03:55 +0900

    fix: 미완성된 부분 commit 제외

--- iOS/FlipMate/FlipMate/Presentation/SocialScene/ViewModel/FollowingsViewModel.swift
+++ iOS/FlipMate/FlipMate/Presentation/SocialScene/ViewModel/FollowingsViewModel.swift
@@ -8,20 +8,20 @@
 import Foundation
 import Combine
 
-struct FollowingsViewModelActions {
-    let viewDidFinish: () -> Void
-}
-
-protocol FollowingsViewModelInput {
-    func followingButtonTouched()
-}
-
-protocol FollowingsViewModelOutput {
-    var tableViewDataSourcePublisher: AnyPublisher<[[String]], Never> { get }
-}
-
-typealias FollowingsViewModelProtocol = FollowingsViewModelInput & FollowingsViewModelOutput
-
-final class FollowingsViewModel: FollowingsViewModelProtocol {
-    
-}
+//struct FollowingsViewModelActions {
+//    let viewDidFinish: () -> Void
+//}
+//
+//protocol FollowingsViewModelInput {
+//    func followingButtonTouched()
+//}
+//
+//protocol FollowingsViewModelOutput {
+//    var tableViewDataSourcePublisher: AnyPublisher<[[String]], Never> { get }
+//}
+//
+//typealias FollowingsViewModelProtocol = FollowingsViewModelInput & FollowingsViewModelOutput
+//
+//final class FollowingsViewModel: FollowingsViewModelProtocol {
+//    
+//}
