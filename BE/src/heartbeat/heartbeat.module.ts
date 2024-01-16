0251bc145b38fa9dcdbab53c75ff2104adb5288e
victolee0 <39608452+victolee0@users.noreply.github.com>
2024-01-16 13:03:33 +0900

    [BE#491] heartbeat가 없는 경우 학습 기록 자동 저장 (#493)
    * feat: heartbeat 30초동안 없는 경우 자동 저장
    
    * feat: 학습중인 친구 우선적으로 불러오기
    
    * docs: 학습 시작 api 문서 수정
    
    * chore: console.log 삭제

--- BE/src/heartbeat/heartbeat.module.ts
+++ BE/src/heartbeat/heartbeat.module.ts
@@ -4,9 +4,10 @@ import { HeartbeatController } from './heartbeat.controller';
 import { AuthModule } from 'src/auth/auth.module';
 import { UsersModule } from 'src/users/users.module';
 import { RedisService } from 'src/common/redis.service';
+import { StudyLogsModule } from 'src/study-logs/study-logs.module';
 
 @Module({
-  imports: [AuthModule, UsersModule],
+  imports: [AuthModule, UsersModule, StudyLogsModule],
   controllers: [HeartbeatController],
   providers: [HeartbeatService, RedisService],
 })
