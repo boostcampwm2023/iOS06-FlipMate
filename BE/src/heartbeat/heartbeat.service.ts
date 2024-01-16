972438f50ff1389007bcd713fa828b2c02d54adc
victolee0 <39608452+victolee0@users.noreply.github.com>
2024-01-16 13:03:33 +0900

    [BE#491] heartbeat가 없는 경우 학습 기록 자동 저장 (#493)
    * feat: heartbeat 30초동안 없는 경우 자동 저장
    
    * feat: 학습중인 친구 우선적으로 불러오기
    
    * docs: 학습 시작 api 문서 수정
    
    * chore: console.log 삭제

--- BE/src/heartbeat/heartbeat.service.ts
+++ BE/src/heartbeat/heartbeat.service.ts
@@ -1,9 +1,23 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Categories } from 'src/categories/categories.entity';
 import { RedisService } from 'src/common/redis.service';
+import { StudyLogs } from 'src/study-logs/study-logs.entity';
+import { StudyLogsService } from 'src/study-logs/study-logs.service';
+import { UsersModel } from 'src/users/entity/users.entity';
+import { Repository } from 'typeorm';
+import moment from 'moment';
 
 @Injectable()
 export class HeartbeatService {
-  constructor(private redisService: RedisService) {}
+  constructor(
+    private redisService: RedisService,
+    @InjectRepository(StudyLogs)
+    private studyLogsRepository: Repository<StudyLogs>,
+    @InjectRepository(UsersModel)
+    private usersRepository: Repository<UsersModel>,
+    private studyLogsService: StudyLogsService,
+  ) {}
 
   recordHeartbeat(userId: number) {
     this.redisService.hset(`${userId}`, 'received_at', `${Date.now()}`);
@@ -19,9 +33,58 @@ export class HeartbeatService {
           'received_at',
         );
         if (now - +received_at > 30000) {
+          await this.removeOldData(clientId);
           await this.redisService.del(`${clientId}`);
+          await this.usersRepository.update(
+            { id: +clientId },
+            { is_studying: false },
+          );
         }
       }
     }, 10000);
+  }
+
+  async removeOldData(clientId: string): Promise<void> {
+    const started_at = await this.redisService.hget(
+      `${clientId}`,
+      'started_at',
+    );
+    const received_at = await this.redisService.hget(
+      `${clientId}`,
+      'received_at',
+    );
+    const category_id = await this.redisService.hget(
+      `${clientId}`,
+      'category_id',
+    );
+
+    const learning_time = moment(+received_at).diff(started_at, 's');
+    const moment_received_at = moment(+received_at);
+    const offset = await this.usersRepository.findOne({
+      select: ['timezone'],
+      where: { id: +clientId },
+    });
+
+    const received_at_with_offset = `${moment_received_at.format(
+      'YYYY-MM-DD HH:mm:ss',
+    )}${offset.timezone}`;
+
+    const learningTimes = this.studyLogsService.calculateLearningTimes(
+      received_at_with_offset,
+      +learning_time,
+    );
+
+    for (const { started_at, date, learning_time } of learningTimes) {
+      const studyLog = this.studyLogsRepository.create({
+        type: 'finish',
+        date,
+        learning_time,
+        created_at: started_at,
+        user_id: { id: +clientId } as UsersModel,
+        category_id: { id: +category_id || null } as Categories,
+        is_finished: false,
+      });
+      await this.studyLogsRepository.save(studyLog);
+    }
   }
 }
