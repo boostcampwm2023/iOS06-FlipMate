276fb3994086837b29a2ed9ef42e6a207f9efe12
leemhyungyu <lhglbkksj@naver.com>
2024-03-02 17:07:29 +0900

    chore: FMCharts Package 생성

---
+++ iOS/FlipMate/FMCharts/Tests/FMChartsTests/FMChartsTests.swift
@@ -0,0 +1,12 @@
+import XCTest
+@testable import FMCharts
+
+final class FMChartsTests: XCTestCase {
+    func testExample() throws {
+        // XCTest Documentation
+        // https://developer.apple.com/documentation/xctest
+
+        // Defining Test Cases and Test Methods
+        // https://developer.apple.com/documentation/xctest/defining_test_cases_and_test_methods
+    }
+}
