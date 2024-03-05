b085068d51ced8ab8e3ea34b9d638017559b9142
leemhyungyu <lhglbkksj@naver.com>
2024-03-05 16:04:29 +0900

    feat: 각 차트 별 ChartData 추가

---
+++ iOS/FlipMate/FMCharts/Sources/FMCharts/Data/Implementations/BarChartData.swift
@@ -0,0 +1,14 @@
+//
+//  BarChartData.swift
+//
+//
+//  Created by 임현규 on 2024/03/02.
+//
+
+import Foundation
+
+final public class BarChartData: ChartData {
+    public override init(entry: ChartDataEntry, label: String) {
+        super.init(entry: entry, label: label)
+    }
+}
---
+++ iOS/FlipMate/FMCharts/Sources/FMCharts/Data/Implementations/PieChartData.swift
@@ -0,0 +1,14 @@
+//
+//  PieChartData.swift
+//
+//
+//  Created by 임현규 on 2024/03/02.
+//
+
+import Foundation
+
+final public class PieChartData: ChartData {
+    public override init(entry: ChartDataEntry, label: String) {
+        super.init(entry: entry, label: label)
+    }
+}
