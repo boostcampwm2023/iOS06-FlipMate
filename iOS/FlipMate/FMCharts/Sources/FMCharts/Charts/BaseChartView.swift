111a9ec4695acd6d445851b7feb0ce0296a8aa60
leemhyungyu <lhglbkksj@naver.com>
2024-03-05 16:16:48 +0900

    feat: BaseChartView 추가

---
+++ iOS/FlipMate/FMCharts/Sources/FMCharts/Charts/BaseChartView.swift
@@ -0,0 +1,34 @@
+//
+//  BaseChartView.swift
+//
+//
+//  Created by 임현규 on 2024/03/02.
+//
+
+import UIKit
+
+public class BaseChartView: UIView {
+    // MARK: - Properties
+    public var data: ChartData?
+    
+    // MARK: - init
+    public override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    required public init?(coder: NSCoder) {
+        fatalError("Don't use StoryBoard")
+    }
+    
+    // MARK: - Life Cycle
+    public override func draw(_ rect: CGRect) {
+        // MARK: - 공통으로 결과 없을 떄 Text 그리기
+    }
+}
+
+// MARK: - Methods
+extension BaseChartView {
+    public func numberOfData() -> Int {
+        return data?.entry.count ?? -1
+    }
+}
