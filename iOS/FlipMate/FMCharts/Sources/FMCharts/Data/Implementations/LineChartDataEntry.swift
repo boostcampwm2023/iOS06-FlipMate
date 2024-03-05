b1f395c3befce6cbaaee864807848b80df82969e
leemhyungyu <lhglbkksj@naver.com>
2024-03-05 15:57:55 +0900

    feat: 차트 종류 별 DataEntry 생성

---
+++ iOS/FlipMate/FMCharts/Sources/FMCharts/Data/Implementations/LineChartDataEntry.swift
@@ -0,0 +1,14 @@
+//
+//  LineChartDataEntry.swift
+//
+//
+//  Created by 임현규 on 2024/03/05.
+//
+
+import Foundation
+
+public class LineChartDataEntry: ChartDataEntry {
+    public override init(xValues: [Double], yValues: [Double]) {
+        super.init(xValues: xValues, yValues: yValues)
+    }
+}
---
+++ iOS/FlipMate/FMCharts/Sources/FMCharts/Data/Implementations/PieChartDataEntry.swift
@@ -0,0 +1,14 @@
+//
+//  PieChartDataEntry.swift
+//
+//
+//  Created by 임현규 on 2024/03/05.
+//
+
+import Foundation
+
+public class PieChartDataEntry: ChartDataEntry {
+    public override init(xValues: [Double], yValues: [Double]) {
+        super.init(xValues: xValues, yValues: yValues)
+    }
+}
