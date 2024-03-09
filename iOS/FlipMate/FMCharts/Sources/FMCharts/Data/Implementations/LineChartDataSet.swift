1fd8d3cc490cfc77d723e4bfac24765d62491d56
leemhyungyu <lhglbkksj@naver.com>
2024-03-09 15:22:41 +0900

    feat: LineChartDataSetProtocol 구현

--- iOS/FlipMate/FMCharts/Sources/FMCharts/Data/Implementations/LineChartDataSet.swift
+++ iOS/FlipMate/FMCharts/Sources/FMCharts/Data/Implementations/LineChartDataSet.swift
@@ -7,7 +7,21 @@
 
 import Foundation
 
-final public class LineChartDataSet: ChartDataSet {
+public protocol LineChartDataSetProtocol {
+    var lineWidth: Double { get }
+    var lineColor: String { get }
+    var dotWidth: Double { get }
+    var dotheight: Double { get }
+    var dotColor: String { get }
+}
+
+final public class LineChartDataSet: ChartDataSet, LineChartDataSetProtocol {
+    public var lineWidth: Double = 3
+    public var lineColor: String = .init()
+    public var dotWidth: Double = .init()
+    public var dotheight: Double = .init()
+    public var dotColor: String = .init()
+    
     public override init(entry: [ChartDataEntry]) {
         super.init(entry: entry)
     }
