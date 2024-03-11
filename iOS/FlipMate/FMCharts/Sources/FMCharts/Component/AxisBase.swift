f654b7d8e2a897ab4d5f63c1f4003e3000857311
leemhyungyu <lhglbkksj@naver.com>
2024-03-11 18:47:55 +0900

    feat: 축 객체 구현
    - xAxis, yAxis, AxisBase 객체 구현

---
+++ iOS/FlipMate/FMCharts/Sources/FMCharts/Component/AxisBase.swift
@@ -0,0 +1,16 @@
+//
+//  AxisBase.swift
+//
+//
+//  Created by 임현규 on 2024/03/02.
+//
+
+import Foundation
+
+open class AxisBase {
+    open var enties: [String] = .init()
+    
+    public init(enties: [String]) {
+        self.enties = enties
+    }
+}
---
+++ iOS/FlipMate/FMCharts/Sources/FMCharts/Component/XAxis.swift
@@ -0,0 +1,18 @@
+//
+//  XAxis.swift
+//
+//
+//  Created by 임현규 on 2024/03/02.
+//
+
+import Foundation
+
+open class XAxis: AxisBase {
+    public override init(enties: [String]) {
+        super.init(enties: enties)
+    }
+    
+    public convenience init(enties: [Double]) {
+        self.init(enties: enties.map { String($0) })
+    }
+}
