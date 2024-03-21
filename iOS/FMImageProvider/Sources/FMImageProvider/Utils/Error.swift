77114e9e336ee7b03aeeedd59af2fd9820b2e779
ericKwon95 <ericyong95@gmail.com>
2024-03-22 04:29:14 +0900

    feat: 에러 상황 추가
    - 메모리 캐시, 디스크 캐시, 이미지 다운로더 각각에 대한 에러 설정

---
+++ iOS/FMImageProvider/Sources/FMImageProvider/Utils/Error.swift
@@ -0,0 +1,49 @@
+//
+//  FMImageProviderError.swift
+//  
+//
+//  Created by 권승용 on 1/22/24.
+//
+
+import Foundation
+
+enum FMImageProviderError: Error, Equatable {
+    enum MemoryCacherError: LocalizedError {
+        case invalidKey
+        
+        var errorDescription: String? {
+            switch self {
+            case .invalidKey:
+                return "no object found by given key"
+            }
+        }
+    }
+    
+    enum DiskCacherError: LocalizedError {
+        case cacheDirectoryNil
+        case createFileFailed
+        case contentLoadFail
+        
+        var errorDescription: String? {
+            switch self {
+            case .cacheDirectoryNil:
+                return "cache directory initializing failed"
+            case .createFileFailed:
+                return "creating file failed from fileManager"
+            case .contentLoadFail:
+                return "content load failed from fileManager"
+            }
+        }
+    }
+    
+    enum ImageDownloaderError: LocalizedError {
+        case noURL
+        
+        var errorDescription: String? {
+            switch self {
+            case .noURL:
+                return "no URL"
+            }
+        }
+    }
+}
