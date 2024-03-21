535b08d8c2f4b2d6ac2b9a488f2f11686d28854e
ericKwon95 <ericyong95@gmail.com>
2024-03-22 04:55:27 +0900

    test: 디스크 캐셔 removeAll 메소드 테스트 코드 작성
    - 파일과 디렉토리의 구분을 위한 로직 구현
    - 딕셔너리와 파일 삭제를 모두 지원하는 removeItem에 대해 이미지 디렉토리 내부에는 파일만 있을 것이기 때문에 Fake 객체는 디렉토리면 전체 삭제, 파일이면 개별 삭제로 구현
    - 범용적으로 사용할 수는 없고 디스크 캐셔 구현 자체에 의존하게 된다. 좋지 않지만 해결 방법이 떠오르지 않음 ㅠ

--- iOS/FMImageProvider/Tests/FMImageProviderTests/DiskCacherTests.swift
+++ iOS/FMImageProvider/Tests/FMImageProviderTests/DiskCacherTests.swift
@@ -8,8 +8,14 @@
 import XCTest
 @testable import FMImageProvider
 
+enum FileType {
+    case directory
+    case file
+}
+
 final class FakeFileManager: FileManager {
     var diskCache: [String:Data] = [:]
+    private var fileTypeCache: [String:FileType] = [:]
     
     override func urls(for directory: FileManager.SearchPathDirectory, in domainMask: FileManager.SearchPathDomainMask) -> [URL] {
         let url = URL(fileURLWithPath: "/path/to/cacheDirectory/")
@@ -18,14 +24,17 @@ final class FakeFileManager: FileManager {
     
     override func createFile(atPath path: String, contents data: Data?, attributes attr: [FileAttributeKey : Any]? = nil) -> Bool {
         diskCache[path] = data
+        fileTypeCache[path] = .file
         return true
     }
     
     override func createDirectory(atPath path: String, withIntermediateDirectories createIntermediates: Bool, attributes: [FileAttributeKey : Any]? = nil) throws {
+        fileTypeCache[path] = .directory
         return
     }
     
     override func createDirectory(at url: URL, withIntermediateDirectories createIntermediates: Bool, attributes: [FileAttributeKey : Any]? = nil) throws {
+        fileTypeCache[url.path] = .directory
         return
     }
     
@@ -35,8 +44,17 @@ final class FakeFileManager: FileManager {
     }
     
     override func removeItem(at URL: URL) throws {
-        
-        diskCache[URL.path] = nil
+        FMLogger.test.log("\(URL.path)")
+        switch fileTypeCache[URL.path] {
+        case .directory:
+            diskCache = [:]
+            fileTypeCache = [:]
+        case .file:
+            diskCache[URL.path] = nil
+            fileTypeCache[URL.path] = nil
+        default:
+            break
+        }
     }
     
     override func fileExists(atPath path: String) -> Bool {
@@ -73,5 +91,14 @@ final class DiskCacherTests: XCTestCase {
         } catch let error {
             XCTAssertEqual(error as? FMImageProviderError.DiskCacherError, .contentLoadFail)
         }
+    }
+    
+    func test_디스크_캐시_removeAll_성공() throws {
+        try sut.save(key: CacheKey.dummy, imageData: ImageData.dummy)
+        
+        try sut.removeAll()
+        
+        let cacheCount = fileManager.diskCache.count
+        XCTAssertEqual(cacheCount, 0)
     }
 }
