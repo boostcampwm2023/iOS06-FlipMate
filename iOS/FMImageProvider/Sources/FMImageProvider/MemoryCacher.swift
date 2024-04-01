02ec4876772d1b1e426e036045ee03d012750cd4
ericKwon95 <ericyong95@gmail.com>
2024-04-01 13:55:38 +0900

    feat: 메모리 캐셔에 cost 계산을 통한 용량 관리 추가

--- iOS/FMImageProvider/Sources/FMImageProvider/MemoryCacher.swift
+++ iOS/FMImageProvider/Sources/FMImageProvider/MemoryCacher.swift
@@ -8,17 +8,27 @@
 import Foundation
 
 final class MemoryCacher: MemoryCacheable {
-    
     private var memoryStorage: NSCache<NSString, NSData>
     
     init(memoryStorage: NSCache<NSString, NSData>) {
+        memoryStorage.name = "FMImageProviderCache"
+        memoryStorage.countLimit = 0
+        memoryStorage.totalCostLimit = 1_000_000_000 // 최대 메모리 캐시 크기 100MB
         self.memoryStorage = memoryStorage
     }
         
+    // MARK: - Interface Methods
+    /// 메모리 캐시에 이미지 데이터를 저장하는 함수
+    /// - Parameters:
+    ///   - url: key로 사용할 이미지 url
+    ///   - imageData: 저장할 이미지 데이터
     func save(key url: String, imageData: Data) {
-        memoryStorage.setObject(NSData(data: imageData), forKey: NSString(string: url))
+        memoryStorage.setObject(NSData(data: imageData), forKey: NSString(string: url), cost: imageData.count)
     }
     
+    /// 메모리 캐시로부터 이미지 데이터를 불러오는 함수
+    /// - Parameter url: key로 사용할 이미지 url
+    /// - Returns: 불러올 이미지 데이터
     func load(key url: String) throws -> Data {
         guard let imageData = memoryStorage.object(forKey: NSString(string: url)) else {
             throw FMImageProviderError.MemoryCacherError.invalidKey
