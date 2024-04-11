388533205e2e0291a4c0a9550c137fc4a4969475
ericKwon95 <ericyong95@gmail.com>
2024-04-11 11:32:50 +0900

    feat: 구현한 LRU캐시를 사용해 캐시 동작 구현
    - 메모리에서 작동한 LRU캐시에 대한 디스크로의 저장 / 불러오기 구현

--- iOS/FMImageProvider/Sources/FMImageProvider/DiskCacher.swift
+++ iOS/FMImageProvider/Sources/FMImageProvider/DiskCacher.swift
@@ -8,18 +8,33 @@
 import Foundation
 import CryptoKit
 
+struct LRUCacheRepresentor: Codable {
+    private var caches: [LRUCacheData] = []
+    
+    mutating func setCaches(with caches: [LRUCacheData]) {
+        self.caches = caches
+    }
+    
+    func getCaches() -> [LRUCacheData] {
+        return caches
+    }
+}
+
 final class DiskCacher: DiskCacheable {
     private let fileManager: FileManager
     private var cacheDirectory: URL?
+    private let lruCache: LRUCache
     
-    init?(fileManager: FileManager) {
+    init?(fileManager: FileManager, capacity: Int) {
         self.fileManager = fileManager
+        self.lruCache = LRUCache(capacity: capacity)
         do {
             self.cacheDirectory = try getCacheDirectoryPath()
         } catch let error {
             FMLogger.general.error("initialize failed: \(error)")
             return nil
         }
+        try? getLRUCacheStatus()
     }
     
     // MARK: - Interface Methods
@@ -29,17 +44,21 @@ final class DiskCacher: DiskCacheable {
     ///   - imageData: 이미지 데이터
     func save(key url: String, imageData: Data) throws {
         let filePath = try getFilePath(forKey: url)
+        lruCache.insert(url, LRUCacheData(cost: imageData.count, key: filePath.path))
         guard fileManager.createFile(atPath: filePath.path, contents: imageData) else {
             throw FMImageProviderError.DiskCacherError.createFileFailed
         }
+        try saveLRUCacheStatus()
     }
     
     /// 주어진 url을 해시 변한한 값을 파일 이름으로 하여 해당 파일을 디스크로부터 불러와 반환
     /// - Parameter url: 이미지 주소 url
     /// - Returns: 불러온 이미지 데이터
     func load(key url: String) throws -> Data {
-        let filePath = try getFilePath(forKey: url)
-        guard let imageData = fileManager.contents(atPath: filePath.path) else {
+        guard let filePath = lruCache.get(url)?.filePath else {
+            throw FMImageProviderError.DiskCacherError.contentLoadFail
+        }
+        guard let imageData = fileManager.contents(atPath: filePath) else {
             throw FMImageProviderError.DiskCacherError.contentLoadFail
         }
         return imageData
@@ -51,6 +70,7 @@ final class DiskCacher: DiskCacheable {
             throw FMImageProviderError.DiskCacherError.cacheDirectoryNil
         }
         try fileManager.removeItem(at: directoryPath)
+        lruCache.removeAll()
     }
 }
 
@@ -96,6 +116,37 @@ extension DiskCacher {
         let truncatedHexString = String(hexString.prefix(16))
         return truncatedHexString
     }
+    
+    /// LRUCache 표현을 디스크에 JSON 포맷으로 저장하는 함수
+    private func saveLRUCacheStatus() throws {
+        guard let filePath = cacheDirectory?.appendingPathComponent(Constant.lruCacheRepresentorName) else {
+            throw FMImageProviderError.DiskCacherError.cacheDirectoryNil
+        }
+        let arr = lruCache.makeArray()
+
+        var cacheRepresenter = LRUCacheRepresentor()
+        cacheRepresenter.setCaches(with: arr)
+        
+        let data = try JSONEncoder().encode(cacheRepresenter)
+        fileManager.createFile(atPath: filePath.path, contents: data)
+    }
+    
+    /// 디스크로부터 LRUCache 표현을 불러와 LRUCache 인스턴스에 저장하는 함수
+    private func getLRUCacheStatus() throws {
+        guard let filePath = cacheDirectory?.appendingPathComponent(Constant.lruCacheRepresentorName) else {
+            // LRUCache 파일이 없을 수 있기 때문에 오류 던지기 대신 함수 종료
+            return
+        }
+        guard let fileData = fileManager.contents(atPath: filePath.path) else {
+            throw FMImageProviderError.DiskCacherError.contentLoadFail
+        }
+        
+        let lruRepresentor = try JSONDecoder().decode(LRUCacheRepresentor.self, from: fileData)
+        let caches = lruRepresentor.getCaches()
+        lruCache.initWithArr(caches)
+    }
+}
+
 enum Constant {
     static let cacheDirectoryName = "FlipMateImages"
     static let lruCacheRepresentorName = "CacheRepresentor"
