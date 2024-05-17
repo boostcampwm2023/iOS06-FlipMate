2106000df1120cd885b33dc669364baa5e328429
leemhyungyu <lhglbkksj@naver.com>
2024-05-17 21:56:56 +0900

    feat: Core 패키지 및 모듈 의존성 추가

--- iOS/FlipMate/FlipMate/DesignSystem/Package.swift
+++ iOS/FlipMate/FlipMate/DesignSystem/Package.swift
@@ -6,16 +6,18 @@ import PackageDescription
 let package = Package(
     name: "DesignSystem",
     products: [
-        // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "DesignSystem",
             targets: ["DesignSystem"]),
     ],
+    
+    dependencies: [
+        .package(path: "../Core")
+    ],
+    
     targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "DesignSystem"),
+            name: "DesignSystem", dependencies: [ .product(name: "Core", package: "Core")]),
         .testTarget(
             name: "DesignSystemTests",
             dependencies: ["DesignSystem"]),
