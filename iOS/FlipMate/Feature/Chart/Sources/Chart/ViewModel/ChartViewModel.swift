ae105fc7cbb61e2f0496ec7cdd0468f46f05e26d
ericKwon95 <ericyong95@gmail.com>
2024-06-02 16:24:09 +0900

    feat: 차트 모듈 생성

---
+++ iOS/FlipMate/Feature/Chart/Sources/Chart/ViewModel/ChartViewModel.swift
@@ -0,0 +1,77 @@
+//
+//  ChartViewModel.swift
+//
+//
+//  Created by 권승용 on 6/2/24.
+//
+
+import Foundation
+import Combine
+import Domain
+
+protocol ChartViewModelInput {
+    func viewDidLoad()
+    func dateDidSelected(date: Date)
+}
+
+protocol ChartViewModelOutput {
+    var dailyChartPublisher: AnyPublisher<StudyLog, Never> { get }
+    var weeklyChartPulisher: AnyPublisher<[DailyData], Never> { get }
+}
+
+typealias ChartViewModelProtocol = ChartViewModelInput & ChartViewModelOutput
+
+final class ChartViewModel: ChartViewModelProtocol {
+    
+    // MARK: - Properties
+    private var selectedDate = Date()
+    
+    // MARK: - UseCase
+    private let dailyChartUseCase: FetchDailyChartUseCase
+    private let weeklyChartUseCase: FetchWeeklyChartUseCase
+    
+    // MARK: - Subject
+    private let dailyChartSubject = PassthroughSubject<StudyLog, Never>()
+    private let weeklyChartSubject = PassthroughSubject<[DailyData], Never>()
+    // MARK: - Publihser
+    var dailyChartPublisher: AnyPublisher<StudyLog, Never> {
+        return dailyChartSubject.eraseToAnyPublisher()
+    }
+    
+    var weeklyChartPulisher: AnyPublisher<[DailyData], Never> {
+        return weeklyChartSubject.eraseToAnyPublisher()
+    }
+    
+    init(dailyChartUseCase: FetchDailyChartUseCase,
+         weeklyChartUseCase: FetchWeeklyChartUseCase) {
+        self.dailyChartUseCase = dailyChartUseCase
+        self.weeklyChartUseCase = weeklyChartUseCase
+    }
+    
+    // MARK: - input
+    func viewDidLoad() {
+        fetchDailyChartLog(at: selectedDate)
+        fetchWeeklyChartLog(at: selectedDate)
+    }
+    
+    func dateDidSelected(date: Date) {
+        selectedDate = date
+        fetchDailyChartLog(at: selectedDate)
+    }
+}
+
+private extension ChartViewModel {
+    func fetchDailyChartLog(at date: Date) {
+        Task {
+            let log = try await dailyChartUseCase.fetchDailyChartLog(at: date)
+            dailyChartSubject.send(log.studyLog)
+        }
+    }
+    
+    func fetchWeeklyChartLog(at date: Date) {
+        Task {
+            let log = try await weeklyChartUseCase.fetchWeeklyChartLog(at: date)
+            weeklyChartSubject.send(log.dailyData)
+        }
+    }
+}
