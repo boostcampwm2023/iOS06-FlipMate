83b34a969bdbc89dda3d7dcd0f500f8403969a55
leemhyungyu <lhglbkksj@naver.com>
2024-06-16 14:36:17 +0900

    feat: CategorySettingCollectionViewCell 구현
    - Category Feature에서 사용하는 새로운 Cell
    - 기존의 CategoryListCollectionViewCell를 재사용하지 않기 위해 Category Feature 내부에 구현

---
+++ iOS/FlipMate/Feature/Category/Sources/Category/View/CategorySettingCollectionViewCell.swift
@@ -0,0 +1,49 @@
+//
+//  CategorySettingCollectionViewCell.swift
+//
+//
+//  Created by 임현규 on 6/16/24.
+//
+
+import UIKit
+import DesignSystem
+
+public final class CategorySettingCollectionViewCell: UICollectionViewCell {
+    
+    // MARK: - UI Components
+    private let categoryView: CategoryInfoView = {
+        let view = CategoryInfoView(isTimerLabelHidden: true)
+        view.translatesAutoresizingMaskIntoConstraints = false
+        view.backgroundColor = .systemBackground
+        return view
+    }()
+    
+    // MARK: - init
+    public override init(frame: CGRect) {
+        super.init(frame: frame)
+        configureUI()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    // MARK: - Public Methods
+    public func updateUI(_ subjectLabelText: String, _ circleBackgroundColor: UIColor?, _ timeLabelText: String?) {
+        categoryView.updateUI(subjectLabelText, circleBackgroundColor, timeLabelText)
+    }
+}
+
+// MARK: - Private Methods
+private extension CategorySettingCollectionViewCell {
+    func configureUI() {
+        [ categoryView ] .forEach { contentView.addSubview($0) }
+        
+        NSLayoutConstraint.activate([
+            categoryView.topAnchor.constraint(equalTo: contentView.topAnchor),
+            categoryView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor),
+            categoryView.trailingAnchor.constraint(equalTo: contentView.trailingAnchor),
+            categoryView.bottomAnchor.constraint(equalTo: contentView.bottomAnchor)
+        ])
+    }
+}
